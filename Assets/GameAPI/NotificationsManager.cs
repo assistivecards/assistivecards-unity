87023aef6d7b8c4435f0eea5f2d0d85649007b0c
Arda Özkardeşler <aozkardesler@gmail.com>
2022-11-16 16:51:51 +0300

    Add local notifications for iOS
    *Fix some issues with repeated Android notifications

--- Assets/GameAPI/NotificationsManager.cs
+++ Assets/GameAPI/NotificationsManager.cs
@@ -1,7 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_IOS
+    using Unity.Notifications.iOS;
+#elif UNITY_ANDROID
 using Unity.Notifications.Android;
+#endif
 using UnityEngine;
 
 public class NotificationsManager : MonoBehaviour
@@ -16,11 +18,13 @@ public class NotificationsManager : MonoBehaviour
 
     private void Start()
     {
-        AndroidNotificationCenter.CancelAllNotifications();
+        // AndroidNotificationCenter.CancelAllDisplayedNotifications();
     }
 
-    void CreateChannel()
+    void SendNotification()
     {
+
+#if UNITY_ANDROID
         var channel = new AndroidNotificationChannel()
         {
             Id = "channel_id",
@@ -30,29 +34,55 @@ public class NotificationsManager : MonoBehaviour
         };
 
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
-    }
 
-    void SendNotification()
-    {
         AndroidNotification notification = new AndroidNotification();
 
         notification.Title = "Hey! We missed you!";
-        notification.Text = "Come back and continue to learn!";
+        notification.Text = "Come back and practice!";
         notification.FireTime = System.DateTime.Now.AddSeconds(15);
         notification.RepeatInterval = new TimeSpan(0, 0, 3, 0);
-        notification.ShowTimestamp = true;
+        // notification.FireTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 9, 0, 0).AddDays(1);
+        // notification.RepeatInterval = reminderPeriod == "Weekly" ? new TimeSpan(7, 0, 0, 0) : new TimeSpan(1, 0, 0, 0);
+        notification.ShouldAutoCancel = true;
 
-        AndroidNotificationCenter.SendNotification(notification, "channel_id");
+        var id = AndroidNotificationCenter.SendNotification(notification, "channel_id");
+
+        if (AndroidNotificationCenter.CheckScheduledNotificationStatus(id) == NotificationStatus.Scheduled)
+        {
+            AndroidNotificationCenter.CancelAllNotifications();
+            AndroidNotificationCenter.SendNotification(notification, "channel_id");
+        }
+#endif
+
+#if UNITY_IOS
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
+        {
+            // TimeInterval = reminderPeriod == "Weekly" ? new TimeSpan(7, 0, 0, 0) : new TimeSpan(1, 0, 0, 0);
+            TimeInterval = new TimeSpan(0,0,3,0),
+            Repeats = true
+        };
+
+        var notificationIOS = new iOSNotification()
+        {
+            Identifier = "_notification_01",
+            Title = "Hey! We missed you!",
+            Body = "",
+            Subtitle = "Come back and practice!",
+            ShowInForeground = true,
+            ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+            CategoryIdentifier = "category_a",
+            ThreadIdentifier = "thread1",
+            Trigger = timeTrigger,
+        };
+
+        iOSNotificationCenter.ScheduleNotification(notificationIOS);
+#endif
     }
 
     private void OnApplicationFocus(bool focusStatus)
     {
         if (focusStatus == false)
-        {
-            CreateChannel();
             SendNotification();
-        }
-
     }
 
     private void Update()

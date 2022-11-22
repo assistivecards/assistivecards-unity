8f1b7da2452aa40409bafe7af35353b5a6fca6eb
Arda Özkardeşler <aozkardesler@gmail.com>
2022-11-22 16:04:31 +0300

    Separate IAP Manager and UI Manager scripts

--- Assets/Scripts/In App Purchase/IAPManager.cs
+++ Assets/Scripts/In App Purchase/IAPManager.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Purchasing;
-using TMPro;
-using System.Collections.Generic;
 
 public class IAPManager : MonoBehaviour
 {
     private string premium = "com.assistivecards.unity-template.premium";
-    [SerializeField] private GameObject restoreButton;
+
     GameAPI gameAPI;
-    [SerializeField] TMP_InputField availablePacksText;
-    [SerializeField] List<AssistiveCardsSDK.AssistiveCardsSDK.Pack> availablePacksArray;
-    [SerializeField] Button premiumButton;
+    [SerializeField] IAPUIManager IAPUIManager;
 
 
     [SerializeField] private Text currencySymbol;
@@ -21,19 +17,8 @@ public class IAPManager : MonoBehaviour
     private void Awake()
     {
         gameAPI = Camera.main.GetComponent<GameAPI>();
-        if (Application.platform != RuntimePlatform.IPhonePlayer)
-        {
-            restoreButton.SetActive(false);
-        }
     }
 
-    private async void Start()
-    {
-        CheckIfPremiumButtonInteractable();
-        await GameAPI.cacheData;
-        GetAvailablePacks();
-
-    }
     public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
     {
         m_StoreController = controller;
@@ -57,53 +42,13 @@ public class IAPManager : MonoBehaviour
         if (product.definition.id == premium)
         {
             gameAPI.SetPremium("A5515T1V3C4RD5");
-            CheckIfPremiumButtonInteractable();
-            availablePacksArray.Clear();
-            availablePacksText.text = "";
-            GetAvailablePacks();
+            IAPUIManager.CheckIfPremiumButtonInteractable();
+            IAPUIManager.ResetAvailablePacks();
         }
     }
 
     public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
     {
         Debug.Log("purchase failed");
-    }
-
-    public void GetAvailablePacks()
-    {
-        if (gameAPI.GetPremium() == "A5515T1V3C4RD5")
-        {
-            foreach (var pack in gameAPI.cachedPacks.packs)
-            {
-                availablePacksArray.Add(pack);
-            }
-        }
-        else
-        {
-            foreach (var pack in gameAPI.cachedPacks.packs)
-            {
-                if (pack.premium == 0)
-                {
-                    availablePacksArray.Add(pack);
-                }
-            }
-        }
-
-        foreach (var pack in availablePacksArray)
-        {
-            availablePacksText.text += pack.slug + "\n";
-        }
-    }
-
-    public void CheckIfPremiumButtonInteractable()
-    {
-        if (gameAPI.GetPremium() == "A5515T1V3C4RD5")
-        {
-            premiumButton.interactable = false;
-        }
-        else
-        {
-            premiumButton.interactable = true;
-        }
     }
 }
---
+++ Assets/Scripts/In App Purchase/IAPUIManager.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class IAPUIManager : MonoBehaviour
+{
+    GameAPI gameAPI;
+    [SerializeField] TMP_InputField availablePacksText;
+    [SerializeField] private GameObject restoreButton;
+    [SerializeField] Button premiumButton;
+
+
+    [SerializeField] List<AssistiveCardsSDK.AssistiveCardsSDK.Pack> availablePacksArray;
+
+    private void Awake()
+    {
+        gameAPI = Camera.main.GetComponent<GameAPI>();
+        if (Application.platform != RuntimePlatform.IPhonePlayer)
+        {
+            restoreButton.SetActive(false);
+        }
+    }
+    private async void Start()
+    {
+        CheckIfPremiumButtonInteractable();
+        await GameAPI.cacheData;
+        GetAvailablePacks();
+    }
+
+    public void GetAvailablePacks()
+    {
+        if (gameAPI.GetPremium() == "A5515T1V3C4RD5")
+        {
+            foreach (var pack in gameAPI.cachedPacks.packs)
+            {
+                availablePacksArray.Add(pack);
+            }
+        }
+        else
+        {
+            foreach (var pack in gameAPI.cachedPacks.packs)
+            {
+                if (pack.premium == 0)
+                {
+                    availablePacksArray.Add(pack);
+                }
+            }
+        }
+
+        foreach (var pack in availablePacksArray)
+        {
+            availablePacksText.text += pack.slug + "\n";
+        }
+    }
+
+    public void CheckIfPremiumButtonInteractable()
+    {
+        if (gameAPI.GetPremium() == "A5515T1V3C4RD5")
+        {
+            premiumButton.interactable = false;
+        }
+        else
+        {
+            premiumButton.interactable = true;
+        }
+    }
+
+    public void ResetAvailablePacks()
+    {
+        availablePacksArray.Clear();
+        availablePacksText.text = "";
+        GetAvailablePacks();
+    }
+}

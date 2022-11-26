abca584a887c68d3c850db29bf56f9966577de26
Özlem <88405137+ogucukbel@users.noreply.github.com>
2022-11-26 06:53:27 +0300

    Change Webview Margin

--- Assets/Scripts/Webview/SampleWebView.cs
+++ Assets/Scripts/Webview/SampleWebView.cs
@@ -148,7 +148,7 @@ public class SampleWebView : MonoBehaviour
 
         //webViewObject.SetScrollbarsVisibility(true);
 
-        webViewObject.SetMargins(Screen.width / 24, Screen.height / 5, Screen.width / 24, 0);
+        webViewObject.SetMargins(Screen.width / 24, Screen.height / 4, Screen.width / 24, 0);
         webViewObject.SetTextZoom(100);  // android only. cf. https://stackoverflow.com/questions/21647641/android-webview-set-font-size-system-default/47017410#47017410
         webViewObject.SetVisibility(true);
 

af15c2bc32e8f90dc365dd36b11f08bb3bdf7778
Arda Özkardeşler <aozkardesler@gmail.com>
2022-12-01 17:07:25 +0300

    Customize "Scenes in Build" on build button click

--- Assets/Editor/CustomBuildProcessor.cs
+++ Assets/Editor/CustomBuildProcessor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -12,18 +13,20 @@ class CustomBuildProcessor : IPreprocessBuildWithReport
     public static string productName;
     public static string productVersion;
     public int callbackOrder { get { return 0; } }
+
+    static CustomBuildProcessor()
+    {
+        productName = PlayerSettings.productName;
+        productVersion = PlayerSettings.bundleVersion;
+        Debug.Log("Current product name is: " + PlayerSettings.productName);
+        Debug.Log("Current product version is: " + PlayerSettings.bundleVersion);
+        BuildPlayerWindow.RegisterBuildPlayerHandler(OnClickBuildPlayer);
+    }
+
     public void OnPreprocessBuild(BuildReport report)
     {
         if (productName != PlayerSettings.productName)
         {
-            Debug.Log("isim değişti");
-
-            // List<EditorBuildSettingsScene> editorBuildSettingsScenesList = new List<EditorBuildSettingsScene>();
-            // var sceneToAdd = new EditorBuildSettingsScene("Assets/Scenes/" + ToTitleCase(PlayerSettings.productName) + ".unity", true);
-            // editorBuildSettingsScenesList.Add(sceneToAdd);
-            // EditorBuildSettings.scenes = editorBuildSettingsScenesList.ToArray();
-            // AssetDatabase.SaveAssets();
-
             Texture2D icon = Resources.Load<Texture2D>(PlayerSettings.productName + "_icon");
             PlayerSettings.SetIcons(NamedBuildTarget.Unknown, new Texture2D[] { icon }, IconKind.Any);
         }
@@ -34,7 +37,6 @@ class CustomBuildProcessor : IPreprocessBuildWithReport
 
         if (productVersion != PlayerSettings.bundleVersion)
         {
-            Debug.Log("bundle version değişti");
             var bundleVersionCode = PlayerSettings.bundleVersion.Replace(".", string.Empty);
             PlayerSettings.Android.bundleVersionCode = Int32.Parse(bundleVersionCode);
         }
@@ -44,10 +46,40 @@ class CustomBuildProcessor : IPreprocessBuildWithReport
         }
     }
 
-    string ToTitleCase(string stringToConvert)
+    static string ToTitleCase(string stringToConvert)
     {
         string firstChar = stringToConvert[0].ToString();
         return (stringToConvert.Length > 0 ? firstChar.ToUpper() + stringToConvert.Substring(1) : stringToConvert);
 
     }
+
+    private static void OnClickBuildPlayer(BuildPlayerOptions options)
+    {
+        if (Application.unityVersion.StartsWith("2022"))
+        {
+            if (productName != PlayerSettings.productName)
+            {
+                List<EditorBuildSettingsScene> editorBuildSettingsScenesList = new List<EditorBuildSettingsScene>();
+                var sceneToAdd = new EditorBuildSettingsScene("Assets/Scenes/" + ToTitleCase(PlayerSettings.productName) + ".unity", true);
+                editorBuildSettingsScenesList.Add(sceneToAdd);
+                EditorBuildSettings.scenes = editorBuildSettingsScenesList.ToArray();
+                AssetDatabase.SaveAssets();
+
+                Array.Clear(options.scenes, 0, options.scenes.Length);
+                options.scenes = EditorBuildSettings.scenes.Select(ebss => ebss.path).ToArray();
+                BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
+            }
+            else
+            {
+                BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
+            }
+        }
+        else
+        {
+            Debug.Log("Update Unity Version to 2022");
+            BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
+        }
+
+    }
+
 }

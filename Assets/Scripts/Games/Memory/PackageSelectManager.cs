8c94ff4c691482fd6a2c8ce7866437e0cdf871f2
Özlem <88405137+ogucukbel@users.noreply.github.com>
2022-12-21 19:55:35 +0300

    Add Transition Screen Load Bar

--- Assets/Scripts/Games/Memory/PackageSelectManager.cs
+++ Assets/Scripts/Games/Memory/PackageSelectManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PackageSelectManager : MonoBehaviour
 {
+    [SerializeField] private TransitionScreenManager transitionScreenManager;
     [SerializeField] private BoardGenerator boardGenerator;
     [SerializeField] private PackSelectionPanel packSelectionPanel;
     private string selectedPack;
 
     public async void OnPackSelect()
     {
+        transitionScreenManager.IncrementProgress(1);
         selectedPack = packSelectionPanel.selectedPackElement.name;
         await boardGenerator.CacheCards(selectedPack);
         await boardGenerator.GenerateRandomBoardAsync(selectedPack);
---
+++ Assets/Scripts/Games/Memory/TransitionScreenManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionScreenManager : MonoBehaviour
+{
+    [SerializeField] private Slider loadingBar;
+    private float fillspeed = 0.2f;
+    private float targetProgress = 0;
+
+    private void Update()
+    {
+        if(loadingBar.value < targetProgress)
+        loadingBar.value += fillspeed * Time.deltaTime;
+    }
+
+    public void IncrementProgress(float newProgess)
+    {
+        targetProgress = loadingBar.value + newProgess;
+    }
+}

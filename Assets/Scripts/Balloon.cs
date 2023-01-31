9e72d299201c5ca4f4498f38a5b342a865492625
Arda Özkardeşler <aozkardesler@gmail.com>
2023-01-31 11:56:34 +0300

    Update Balloon & LevelChangeScreen prefabs

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Balloon : MonoBehaviour
+public class Balloon : MonoBehaviour, IPointerClickHandler
 {
-    private void OnMouseUpAsButton()
+    public void OnPointerClick(PointerEventData eventData)
     {
         gameObject.GetComponent<Animator>().enabled = true;
+        Destroy(gameObject, .5f);
     }
 }
--- Assets/Scripts/BalloonSpawner.cs
+++ Assets/Scripts/BalloonSpawner.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BalloonSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject balloonPrefab;
     public Color[] colors;
+    private GameObject gameCanvas;
     private void OnEnable()
     {
+        gameCanvas = GameObject.Find("GameCanvas");
+        Vector3[] worldCorners = new Vector3[4];
+        gameCanvas.GetComponent<RectTransform>().GetWorldCorners(worldCorners);
+
         for (int i = 0; i < 5; i++)
         {
-            var randomValue = Random.Range(-8, 8);
-            var balloon = Instantiate(balloonPrefab, new Vector3(randomValue, -7, 0), Quaternion.identity);
-            balloon.GetComponent<SpriteRenderer>().color = colors[Random.Range(0, colors.Length)];
-            LeanTween.move(balloon, new Vector3(randomValue, 7, 0), Random.Range(2, 5));
+            var randomValue = Random.Range(worldCorners[0].x, worldCorners[3].x);
+            var balloon = Instantiate(balloonPrefab, new Vector3(randomValue, worldCorners[0].y, 0), Quaternion.identity);
+            balloon.transform.SetParent(gameCanvas.transform);
+            balloon.transform.localScale = Vector3.one * 2.5f;
+            balloon.GetComponent<Image>().color = colors[Random.Range(0, colors.Length)];
+            LeanTween.move(balloon, new Vector3(randomValue, worldCorners[1].y, 0), Random.Range(2, 5));
         }
 
         Invoke("DestroyBalloons", 5f);

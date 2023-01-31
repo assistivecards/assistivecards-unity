e1c6b47fae2a466ddfa55f355b8e3e1a4a38a9ff
Arda Özkardeşler <aozkardesler@gmail.com>
2023-01-31 16:30:38 +0300

    Add pop sfx & update balloon spawner in Silhouette

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -5,9 +5,18 @@ using UnityEngine.EventSystems;
 
 public class Balloon : MonoBehaviour, IPointerClickHandler
 {
+    private GameAPI gameAPI;
+
+    private void Awake()
+    {
+        gameAPI = Camera.main.GetComponent<GameAPI>();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         gameObject.GetComponent<Animator>().enabled = true;
+        gameAPI.PlaySFX("Pop");
+        gameAPI.VibrateWeak();
         Destroy(gameObject, .5f);
     }
 }

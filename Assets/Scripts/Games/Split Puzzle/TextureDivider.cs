629af71f1ace9067363ca385cb765f01a7d917c0
Arda Özkardeşler <aozkardesler@gmail.com>
2023-02-01 16:55:21 +0300

    Place pieces into 4 different slots randomly

--- Assets/Scripts/Games/Split Puzzle/TextureDivider.cs
+++ Assets/Scripts/Games/Split Puzzle/TextureDivider.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class TextureDivider : MonoBehaviour
 {
     [SerializeField] Texture2D textureToDivide;
+    [SerializeField] List<Image> images = new List<Image>();
+    [SerializeField] List<Image> hintImages = new List<Image>();
     [SerializeField] List<Sprite> pieces = new List<Sprite>();
 
     private void Start()
@@ -20,6 +22,22 @@ public class TextureDivider : MonoBehaviour
             {
                 Sprite newSprite = Sprite.Create(texture, new Rect(i * 128, j * 128, 128, 128), new Vector2(0.5f, 0.5f));
                 pieces.Add(newSprite);
+            }
+        }
+
+        for (int i = 0; i < hintImages.Count; i++)
+        {
+            hintImages[i].sprite = pieces[i];
+        }
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            if (images[i].sprite == null)
+            {
+                var randomIndex = Random.Range(0, pieces.Count);
+                var sprite = pieces[randomIndex];
+                pieces.RemoveAt(randomIndex);
+                images[i].sprite = sprite;
             }
         }
 

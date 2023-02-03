02a9e29d71c8d9ad7ba131694139b17cc17db102
Arda Özkardeşler <aozkardesler@gmail.com>
2023-02-03 14:12:44 +0300

    Snap piece to slot if the match is correct

--- Assets/Scripts/Games/Split Puzzle/SplitPuzzleMatchDetection.cs
+++ Assets/Scripts/Games/Split Puzzle/SplitPuzzleMatchDetection.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 
 public class SplitPuzzleMatchDetection : MonoBehaviour, IPointerUpHandler
 {
+    private GameAPI gameAPI;
     private bool isMatched = false;
+    private Transform matchedSlotTransform;
+
+    private void Awake()
+    {
+        gameAPI = Camera.main.GetComponent<GameAPI>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "HintPiece" && other.GetComponent<Image>().sprite == transform.GetChild(1).GetComponent<Image>().sprite)
         {
+            matchedSlotTransform = other.transform;
             isMatched = true;
         }
 
@@ -29,7 +37,9 @@ public class SplitPuzzleMatchDetection : MonoBehaviour, IPointerUpHandler
     {
         if (isMatched)
         {
-            Debug.Log("Correct Match!");
+            gameObject.GetComponent<DraggablePiece>().enabled = false;
+            LeanTween.move(gameObject, matchedSlotTransform.position, 0.25f);
+            gameAPI.PlaySFX("Success");
         }
         else
             Debug.Log("Wrong Match!");

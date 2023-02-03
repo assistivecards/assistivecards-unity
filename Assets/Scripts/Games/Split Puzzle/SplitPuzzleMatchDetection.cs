2452cccf447411a557480228c4ddc9a896860948
Arda Özkardeşler <aozkardesler@gmail.com>
2023-02-03 19:07:49 +0300

    Generate new board if the puzzle is complete

--- Assets/Scripts/Games/Split Puzzle/SplitPuzzleMatchDetection.cs
+++ Assets/Scripts/Games/Split Puzzle/SplitPuzzleMatchDetection.cs
@@ -7,9 +7,14 @@ using UnityEngine.UI;
 public class SplitPuzzleMatchDetection : MonoBehaviour, IPointerUpHandler
 {
     private PuzzleProgressChecker puzzleProgressChecker;
+    private SplitPuzzleBoardGenerator puzzleBoard;
     private GameAPI gameAPI;
-    private bool isMatched = false;
+    public bool isMatched = false;
     private Transform matchedSlotTransform;
+    private GameObject darkSlotsParent;
+    private GameObject lightSlotsParent;
+    [SerializeField] List<GameObject> puzzlePieceParents = new List<GameObject>();
+    [SerializeField] GameObject hintImageParent;
 
     private void Awake()
     {
@@ -19,6 +24,10 @@ public class SplitPuzzleMatchDetection : MonoBehaviour, IPointerUpHandler
     private void Start()
     {
         puzzleProgressChecker = GameObject.Find("GamePanel").GetComponent<PuzzleProgressChecker>();
+        puzzleBoard = GameObject.Find("GamePanel").GetComponent<SplitPuzzleBoardGenerator>();
+        darkSlotsParent = GameObject.Find("PuzzleSlotsDark");
+        lightSlotsParent = GameObject.Find("PuzzleSlotsLight");
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -51,10 +60,31 @@ public class SplitPuzzleMatchDetection : MonoBehaviour, IPointerUpHandler
             {
                 Debug.Log("Puzzle completed!");
                 puzzleProgressChecker.puzzlesCompleted++;
+                puzzleProgressChecker.correctMatches = 0;
+                for (int i = 0; i < puzzlePieceParents.Count; i++)
+                {
+                    puzzlePieceParents[i].transform.SetParent(hintImageParent.transform);
+                    puzzlePieceParents[i].GetComponent<SplitPuzzleMatchDetection>().isMatched = false;
+                }
+                LeanTween.alpha(lightSlotsParent.GetComponent<RectTransform>(), 0, .5f);
+                LeanTween.alpha(darkSlotsParent.GetComponent<RectTransform>(), 0, .25f);
+
+                Invoke("ScaleHintImageDown", .5f);
+                puzzleBoard.Invoke("ClearBoard", 1f);
+                puzzleBoard.Invoke("GenerateRandomBoardAsync", 1f);
+
+
             }
         }
         else
             Debug.Log("Wrong Match!");
+    }
+
+    public void ScaleHintImageDown()
+    {
+        LeanTween.scale(hintImageParent, Vector3.zero, .25f);
+        LeanTween.scale(darkSlotsParent, Vector3.zero, .1f);
+        LeanTween.scale(lightSlotsParent, Vector3.zero, .1f);
     }
 
 }

0d6f874eb6a048cdb90abb5d76e36befb8104ab6
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-02-21 14:32:14 +0300

    Add Drag Card Mechanic

--- Assets/Scripts/Games/Hatch&Match/CardElementHatchMatch.cs
+++ Assets/Scripts/Games/Hatch&Match/CardElementHatchMatch.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CardElementHatchMatch : MonoBehaviour
+public class CardElementHatchMatch : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     private EggController eggController;
-
 
     private void Start() 
     {
         eggController = FindObjectOfType<EggController>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
     }
 }

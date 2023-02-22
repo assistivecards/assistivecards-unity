5b5c3fb237743ef14e0020fe17e9f19e2de38c92
Arda Özkardeşler <aozkardesler@gmail.com>
2023-02-22 12:30:40 +0300

    Add correct & wrong match animations

--- Assets/Scripts/Games/Draw Lines/DrawLinesMatchDetection.cs
+++ Assets/Scripts/Games/Draw Lines/DrawLinesMatchDetection.cs
@@ -8,22 +8,35 @@ public class DrawLinesMatchDetection : MonoBehaviour
 {
     public bool isMatched = false;
     [SerializeField] Image cardToBeMatched;
+    private GameObject matchedOption;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        matchedOption = other.gameObject;
         if (other.tag == "Option" && other.GetComponent<Image>().sprite == cardToBeMatched.sprite)
         {
             isMatched = true;
             Debug.Log("Correct Match!");
             gameObject.GetComponent<DragHandle>().enabled = false;
+            LeanTween.scale(gameObject, Vector3.zero, .25f);
+            Invoke("DisableCurrentHandle", 0.25f);
+            LeanTween.scale(matchedOption, Vector3.one * 1.25f, .25f);
         }
         else
         {
             isMatched = false;
             Debug.Log("Wrong Match!");
             gameObject.GetComponent<DragHandle>().enabled = false;
+            LeanTween.scale(gameObject, Vector3.zero, .25f);
+            Invoke("DisableCurrentHandle", 0.25f);
+            LeanTween.alpha(matchedOption.GetComponent<RectTransform>(), .5f, .25f);
         }
 
+    }
+
+    public void DisableCurrentHandle()
+    {
+        gameObject.SetActive(false);
     }
 
 }

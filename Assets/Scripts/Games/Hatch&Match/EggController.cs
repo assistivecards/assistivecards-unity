0a8ae3eb0ddea8188d8c90f67ffeb2a4299afce1
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-02-27 18:33:08 +0300

    Add Egg Animations to Hatch Match

--- Assets/Scripts/Games/Hatch&Match/EggController.cs
+++ Assets/Scripts/Games/Hatch&Match/EggController.cs
@@ -8,15 +8,23 @@ public class EggController : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 {
     [SerializeField] private BoardCreatorHatchMatch boardCreatorHatchMatch;
 
+    [SerializeField] private Sprite eggPhase0;
     [SerializeField] private Sprite eggPhase1;
     [SerializeField] private Sprite eggPhase2;
     [SerializeField] private Sprite eggPhase3;
     [SerializeField] private Sprite eggPhase4;
+
+    private Animator animator;
     private GameObject card;
 
     private Sprite eggPhaseImage;
 
     public int clickCount;
+
+    private void Awake() 
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void OnPointerDown(PointerEventData pointerEventData)
     {
@@ -32,27 +40,38 @@ public class EggController : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         {
             card = FindObjectOfType<CardElementHatchMatch>().gameObject;
 
-            if(clickCount >= 1 && clickCount < 2)
+            if(clickCount == 1)
             {
+                animator.SetFloat("Phase", 1);
                 GetComponent<Image>().sprite = eggPhase1;
             }
-            else if(clickCount >= 2 && clickCount < 3)
+            else if(clickCount == 2)
             {
+                animator.SetFloat("Phase", 2);
                 GetComponent<Image>().sprite = eggPhase2;
             }
-            else if(clickCount >= 3 && clickCount < 4)
+            else if(clickCount == 3)
             {
+                animator.SetFloat("Phase", 3);
                 GetComponent<Image>().sprite = eggPhase3;
             }
-            else if(clickCount >= 4 && clickCount < 5)
+            else if(clickCount == 4)
             {
+                animator.SetFloat("Phase", 4);
                 GetComponent<Image>().sprite = eggPhase4;
             }
             else if(clickCount >= 5)
             {
-                LeanTween.scale(this.gameObject, Vector3.zero, 0.1f);
-                LeanTween.scale(card, Vector3.one * 0.5f, 0.1f);
+                LeanTween.scale(this.gameObject, Vector3.zero, 0.5f).setOnComplete(ResetEgg);
+                LeanTween.scale(card, Vector3.one * 0.5f, 0.5f);
+                animator.SetFloat("Phase", 6);
             }
         }
+    }
+
+    private void ResetEgg()
+    {
+        animator.SetFloat("Phase", 0);
+        GetComponent<Image>().sprite = eggPhase0;
     }
 }

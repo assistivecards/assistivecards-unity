a62369f82cb99ed7b5c624cd50d4334992f90282
Arda Özkardeşler <aozkardesler@gmail.com>
2023-03-01 11:44:49 +0300

    Generate random board corresponding to selected pack

---
+++ Assets/Scripts/Games/Scratcher/ScratcherBoardGenerator.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public class ScratcherBoardGenerator : MonoBehaviour
+{
+    private GameAPI gameAPI;
+    [SerializeField] Image[] cardImagesInScene;
+    [SerializeField] GameObject[] scratchParents;
+    [SerializeField] AssistiveCardsSDK.AssistiveCardsSDK.Cards cachedCards;
+    [SerializeField] List<AssistiveCardsSDK.AssistiveCardsSDK.Card> randomCards = new List<AssistiveCardsSDK.AssistiveCardsSDK.Card>();
+    [SerializeField] List<Texture2D> randomImages = new List<Texture2D>();
+    [SerializeField] List<Sprite> randomSprites = new List<Sprite>();
+    public string selectedLangCode;
+    [SerializeField] string correctCardSlug;
+    [SerializeField] TMP_Text findText;
+    public string packSlug;
+    [SerializeField] GameObject backButton;
+    public static bool didLanguageChange = true;
+    public static bool isBackAfterSignOut = false;
+
+    private void Awake()
+    {
+        gameAPI = Camera.main.GetComponent<GameAPI>();
+    }
+
+    private void Start()
+    {
+        gameAPI.PlayMusic();
+    }
+
+    private void OnEnable()
+    {
+        if (isBackAfterSignOut)
+        {
+            gameAPI.PlayMusic();
+            // UIController.OnBackButtonClick();
+            isBackAfterSignOut = false;
+        }
+    }
+
+    public async Task CacheCards(string packName)
+    {
+        selectedLangCode = await gameAPI.GetSystemLanguageCode();
+        cachedCards = await gameAPI.GetCards(selectedLangCode, packName);
+
+        if (packName == "shapes")
+        {
+            var cardsList = cachedCards.cards.ToList();
+            cardsList.RemoveAt(8);
+            cardsList.RemoveAt(12);
+            cachedCards.cards = cardsList.ToArray();
+        }
+    }
+
+
+    public async Task GenerateRandomBoardAsync()
+    {
+        if (didLanguageChange)
+        {
+            await CacheCards(packSlug);
+            didLanguageChange = false;
+        }
+
+        for (int i = 0; i < cardImagesInScene.Length; i++)
+        {
+            var cardToAdd = cachedCards.cards[Random.Range(0, cachedCards.cards.Length)];
+            CheckIfCardExists(cardToAdd);
+
+            randomImages.Add(await gameAPI.GetCardImage(packSlug, randomCards[i].slug));
+            randomImages[i].wrapMode = TextureWrapMode.Clamp;
+            randomImages[i].filterMode = FilterMode.Bilinear;
+            randomSprites.Add(Sprite.Create(randomImages[i], new Rect(0.0f, 0.0f, randomImages[i].width, randomImages[i].height), new Vector2(0.5f, 0.5f), 100.0f));
+        }
+
+        correctCardSlug = randomCards[0].slug;
+        findText.text = gameAPI.Translate(findText.gameObject.name, gameAPI.ToSentenceCase(randomCards[0].title).Replace("-", " "), selectedLangCode);
+        var correctCardImageIndex = Random.Range(0, cardImagesInScene.Length);
+        cardImagesInScene[correctCardImageIndex].sprite = randomSprites[0];
+
+        for (int i = 0; i < cardImagesInScene.Length; i++)
+        {
+            cardImagesInScene[i].gameObject.SetActive(true);
+            if (cardImagesInScene[i].sprite == null)
+            {
+                var randomIndex = Random.Range(1, randomSprites.Count);
+                var sprite = randomSprites[randomIndex];
+                randomSprites.RemoveAt(randomIndex);
+                cardImagesInScene[i].sprite = sprite;
+            }
+        }
+
+        ScaleImagesUp();
+        backButton.SetActive(true);
+        Invoke("EnableBackButton", 0.15f);
+    }
+
+    public void ClearBoard()
+    {
+        findText.text = "";
+        randomCards.Clear();
+        randomImages.Clear();
+        randomSprites.Clear();
+        for (int i = 0; i < cardImagesInScene.Length; i++)
+        {
+            cardImagesInScene[i].sprite = null;
+        }
+
+    }
+
+    public void ScaleImagesUp()
+    {
+        LeanTween.scale(findText.gameObject, Vector3.one, 0.15f);
+        for (int i = 0; i < scratchParents.Length; i++)
+        {
+            LeanTween.scale(scratchParents[i].gameObject, Vector3.one, 0.15f);
+        }
+    }
+
+    public void CheckIfCardExists(AssistiveCardsSDK.AssistiveCardsSDK.Card cardToAdd)
+    {
+        if (!randomCards.Contains(cardToAdd) && cardToAdd.slug != correctCardSlug)
+        {
+            randomCards.Add(cardToAdd);
+        }
+        else
+        {
+            cardToAdd = cachedCards.cards[Random.Range(0, cachedCards.cards.Length)];
+            CheckIfCardExists(cardToAdd);
+        }
+    }
+    public void ReadCard()
+    {
+        gameAPI.Speak(randomCards[0].title);
+    }
+
+    public void EnableBackButton()
+    {
+        backButton.GetComponent<Button>().interactable = true;
+    }
+
+
+
+}

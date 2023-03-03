78188da19b74ea3a956844b88bb04846678fd5f8
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-03-03 19:26:47 +0300

    Create Colorful Cards

--- Assets/Scripts/Games/Complete/BoardCreatorComplete.cs
+++ Assets/Scripts/Games/Complete/BoardCreatorComplete.cs
@@ -20,14 +20,21 @@ public class BoardCreatorComplete : MonoBehaviour
     private int randomValue;
     public List<int> randomValueList = new List<int>();
 
+
     [SerializeField] private GameObject cardPrefab;
     public List<GameObject> cards  = new List<GameObject>();
+
+    private List<Transform> cardPositions = new List<Transform>();
+    [SerializeField] private Transform card1Position;
+    [SerializeField] private Transform card2Position;
     public int cardCount;
 
 
     private void OnEnable()
     {
         gameAPI = Camera.main.GetComponent<GameAPI>();
+        cardPositions.Add(card1Position);
+        cardPositions.Add(card2Position);
     }
 
     public async Task CacheCards(string _packSlug)
@@ -81,5 +88,32 @@ public class BoardCreatorComplete : MonoBehaviour
             cards[j].transform.GetChild(0).GetComponent<RawImage>().texture = cardTexture;
             cards[j].transform.GetChild(0).GetComponent<RawImage>().color = Color.black;
         }
+
+        GenerateCards(packSlug, 0);
+        GenerateCards(packSlug, 1);
     }
+
+    public async void GenerateCards(string packSlug, int random)
+    {
+        var cardTexture = await gameAPI.GetCardImage(packSlug, cardNames[randomValueList[random]], 512);
+
+        for(int i = 0; i< cardTextures.cards.Length; i++)
+        {
+            cardNames.Add(cardTextures.cards[i].title.ToLower().Replace(" ", "-"));
+            cardDefinitionsLocale.Add(cardDefinitions.cards[i].title);
+        }
+
+        var card = Instantiate(cardPrefab, cardPositions[random].position, Quaternion.identity);
+
+        cards.Add(card);
+        card.transform.parent = cardPositions[random];
+        card.transform.name = "Card" + random;
+        card.transform.localScale = new Vector3(1.6f, 1.6f, 1.6f);
+
+        cardTexture.wrapMode = TextureWrapMode.Clamp;
+        cardTexture.filterMode = FilterMode.Bilinear;
+
+        card.transform.GetChild(0).GetComponent<RawImage>().texture = cardTexture;
+        card.transform.GetChild(0).transform.localScale = Vector3.one;
+    } 
 }

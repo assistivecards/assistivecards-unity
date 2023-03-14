e9cfd38d2ad61bf1f45d9aefba4564336cc21a11
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-03-14 19:11:50 +0300

    Add Drag&Drop To Cards

---
+++ Assets/Scripts/Games/CardChain/CardControllerCardChain.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CardControllerCardChain : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+}

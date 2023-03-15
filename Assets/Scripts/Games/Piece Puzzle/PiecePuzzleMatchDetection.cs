14c1058a988d60e30ddb0fdeea4d85d032721165
Arda Özkardeşler <aozkardesler@gmail.com>
2023-03-15 16:52:04 +0300

    Detect if the puzzle is complete

--- Assets/Scripts/Games/Piece Puzzle/PiecePuzzleMatchDetection.cs
+++ Assets/Scripts/Games/Piece Puzzle/PiecePuzzleMatchDetection.cs
@@ -6,14 +6,28 @@ using UnityEngine.EventSystems;
 public class PiecePuzzleMatchDetection : MonoBehaviour, IPointerUpHandler
 {
     public bool correctMatch = false;
+    private PuzzleProgressChecker puzzleProgressChecker;
+
+    private void Start()
+    {
+        puzzleProgressChecker = GameObject.Find("GamePanel").GetComponent<PuzzleProgressChecker>();
+    }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         if (correctMatch)
         {
             Debug.Log("Correct Match!");
+            puzzleProgressChecker.correctMatches++;
             LeanTween.move(gameObject, transform.GetChild(0).GetComponent<PiecePuzzleAnchorPointDetection>().matchedTransform.parent.transform.position, .25f);
             transform.SetParent(transform.GetChild(0).GetComponent<PiecePuzzleAnchorPointDetection>().matchedTransform.parent);
+
+            if (puzzleProgressChecker.correctMatches == 4)
+            {
+                Debug.Log("Puzzle completed!");
+                puzzleProgressChecker.puzzlesCompleted++;
+                puzzleProgressChecker.correctMatches = 0;
+            }
         }
         else
         {

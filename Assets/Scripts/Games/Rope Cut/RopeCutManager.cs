8ad0754c8f412f3a17f8d2fa692cd74e1750f8ea
Arda Özkardeşler <aozkardesler@gmail.com>
2023-03-28 17:02:55 +0300

    Fix rope artifact issue on new board generation

--- Assets/Scripts/Games/Rope Cut/RopeCutManager.cs
+++ Assets/Scripts/Games/Rope Cut/RopeCutManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RopeCutManager : MonoBehaviour
 {
+    private int ropeIndex;
+    private Transform hitRope;
 
     // Update is called once per frame
     void Update()
@@ -15,8 +17,18 @@ public class RopeCutManager : MonoBehaviour
             {
                 if (hit.collider.tag == "Rope")
                 {
+                    hitRope = hit.collider.transform.parent;
                     LeanTween.alpha(hit.collider.gameObject, 0, .15f);
                     Destroy(hit.collider.gameObject, .15f);
+
+                    ropeIndex = hit.transform.GetSiblingIndex();
+
+                    for (int i = ropeIndex; i < hitRope.childCount; i++)
+                    {
+                        LeanTween.alpha(hitRope.GetChild(i).gameObject, 0, .25f).setDelay(.25f);
+                        Destroy(hitRope.GetChild(i).gameObject, .5f);
+                    }
+
                 }
             }
         }

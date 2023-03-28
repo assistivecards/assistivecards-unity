5dd0b5a6e2f2fdcec75c1bc728917b39054e91a0
Arda Özkardeşler <aozkardesler@gmail.com>
2023-03-28 13:38:26 +0300

    Add checkpoint panel

--- Assets/Scripts/Games/Rope Cut/RopeCutMatchDetection.cs
+++ Assets/Scripts/Games/Rope Cut/RopeCutMatchDetection.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class RopeCutMatchDetection : MonoBehaviour
 {
     private RopeCutBoardGenerator board;
+    private RopeCutUIController UIController;
 
     private void Start()
     {
         board = GameObject.Find("GamePanel").GetComponent<RopeCutBoardGenerator>();
+        UIController = GameObject.Find("GamePanel").GetComponent<RopeCutUIController>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -16,10 +18,16 @@ public class RopeCutMatchDetection : MonoBehaviour
         if (other.tag == "CorrectCard")
         {
             Debug.Log("Correct Match!");
+            UIController.correctMatches++;
             other.GetComponent<BoxCollider2D>().enabled = false;
             board.Invoke("ScaleImagesDown", 1f);
             board.Invoke("ClearBoard", 1.30f);
-            board.Invoke("GenerateRandomBoardAsync", 1.30f);
+            if (UIController.correctMatches == UIController.checkpointFrequency)
+            {
+                UIController.Invoke("OpenCheckPointPanel", 1.30f);
+            }
+            else
+                board.Invoke("GenerateRandomBoardAsync", 1.30f);
         }
 
         else if (other.tag == "WrongCard")

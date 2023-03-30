99405cb3343a12186a6afb5d9d1dd3f72367b887
Arda Özkardeşler <aozkardesler@gmail.com>
2023-03-30 20:33:34 +0300

    Cut ropes based on first finger touch & swipe

--- Assets/Scripts/Games/Rope Cut/RopeCutManager.cs
+++ Assets/Scripts/Games/Rope Cut/RopeCutManager.cs
@@ -10,25 +10,28 @@ public class RopeCutManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount == 1)
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit.collider != null)
+            if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Moved)
             {
-                if (hit.collider.tag == "Rope")
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
+                if (hit.collider != null)
                 {
-                    hitRope = hit.collider.transform.parent;
-                    LeanTween.alpha(hit.collider.gameObject, 0, .15f);
-                    Destroy(hit.collider.gameObject, .15f);
-
-                    ropeIndex = hit.transform.GetSiblingIndex();
-
-                    for (int i = ropeIndex; i < hitRope.childCount; i++)
+                    if (hit.collider.tag == "Rope")
                     {
-                        LeanTween.alpha(hitRope.GetChild(i).gameObject, 0, .25f).setDelay(.25f);
-                        Destroy(hitRope.GetChild(i).gameObject, .5f);
-                    }
+                        hitRope = hit.collider.transform.parent;
+                        LeanTween.alpha(hit.collider.gameObject, 0, .15f);
+                        Destroy(hit.collider.gameObject, .15f);
 
+                        ropeIndex = hit.transform.GetSiblingIndex();
+
+                        for (int i = ropeIndex; i < hitRope.childCount; i++)
+                        {
+                            LeanTween.alpha(hitRope.GetChild(i).gameObject, 0, .25f).setDelay(.25f);
+                            Destroy(hitRope.GetChild(i).gameObject, .5f);
+                        }
+
+                    }
                 }
             }
         }

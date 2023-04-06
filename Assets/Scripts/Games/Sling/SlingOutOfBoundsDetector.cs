ef3e602a7aa992f6fe4129aeb412d7a24b54e723
Arda Özkardeşler <aozkardesler@gmail.com>
2023-04-06 18:12:02 +0300

    Fix bug on back button click when card is out of bounds

--- Assets/Scripts/Games/Sling/SlingOutOfBoundsDetector.cs
+++ Assets/Scripts/Games/Sling/SlingOutOfBoundsDetector.cs
@@ -1,41 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlingOutOfBoundsDetector : MonoBehaviour
 {
     [SerializeField]
     Transform cardSlot;
     Collider2D collidedCard;
+    SlingUIController UIController;
+    [SerializeField]
+    Transform box;
 
     void Start()
     {
         BoxCollider2D collider = GetComponent<BoxCollider2D>();
         RectTransform rect = GetComponent<RectTransform>();
         collider.size = new Vector2(rect.rect.width, rect.rect.height);
+        UIController = GameObject.Find("GamePanel").GetComponent<SlingUIController>();
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         Debug.Log("Out Of Bounds!!!");
         collidedCard = other;
+        UIController.backButton.GetComponent<Button>().interactable = false;
         Invoke("ResetCardPosition", .25f);
 
     }
 
     void ResetCardPosition()
     {
-        Rigidbody2D rb = collidedCard.GetComponent<Rigidbody2D>();
-        rb.isKinematic = true;
-        rb.velocity = Vector2.zero;
-        rb.freezeRotation = true;
-        collidedCard.transform.localScale = Vector3.zero;
-        collidedCard.transform.rotation = Quaternion.Euler(0, 0, 0);
-        collidedCard.transform.position = cardSlot.position;
-        rb.freezeRotation = false;
-        collidedCard.GetComponent<SwipeManager>().canThrow = true;
-        collidedCard.GetComponent<SwipeManager>().isValid = false;
-        LeanTween.scale(collidedCard.gameObject, Vector3.one * 10, .2f);
+        if (box.localScale == Vector3.one)
+        {
+            Rigidbody2D rb = collidedCard.GetComponent<Rigidbody2D>();
+            rb.isKinematic = true;
+            rb.velocity = Vector2.zero;
+            rb.freezeRotation = true;
+            collidedCard.transform.localScale = Vector3.zero;
+            collidedCard.transform.rotation = Quaternion.Euler(0, 0, 0);
+            collidedCard.transform.position = cardSlot.position;
+            rb.freezeRotation = false;
+            collidedCard.GetComponent<SwipeManager>().canThrow = true;
+            collidedCard.GetComponent<SwipeManager>().isValid = false;
+            LeanTween.scale(collidedCard.gameObject, Vector3.one * 10, .2f);
+            UIController.backButton.GetComponent<Button>().interactable = true;
+        }
+
     }
 
 }

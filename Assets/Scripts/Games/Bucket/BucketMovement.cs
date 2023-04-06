b5b844791f60ee3d72ccf8f4bdcfd2ffc66d2a32
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-04-06 19:56:29 +0300

    Add Limits To Buckets Movement

--- Assets/Scripts/Games/Bucket/BucketMovement.cs
+++ Assets/Scripts/Games/Bucket/BucketMovement.cs
@@ -9,9 +9,24 @@ public class BucketMovement : MonoBehaviour, IDragHandler
     public void OnDrag(PointerEventData eventData) 
     {
         var currentPos = transform.position;
-        if(transform.position.x > 190 && transform.position.y < 1900)
+        Debug.Log(currentPos);
+        if(transform.position.x > 190 && transform.position.x < 1880)
         {
             transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
+        }
+        else if(transform.position.x <= 190)
+        {
+            if(eventData.delta.x > 0)
+            {
+                transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
+            }
+        }
+        else if(transform.position.x >= 1880)
+        {
+            if(eventData.delta.x < 0)
+            {
+                transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
+            }
         }
     }
 }

2112a161055288f4ff48e8b179869a83b7c3ebba
Arda Özkardeşler <aozkardesler@gmail.com>
2023-04-25 11:41:13 +0300

    Add frame indicators

--- Assets/Scripts/Games/Drag Inside/DragInsideMatchDetection.cs
+++ Assets/Scripts/Games/Drag Inside/DragInsideMatchDetection.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragInsideMatchDetection : MonoBehaviour
 {
     [SerializeField] List<GameObject> cardsInside = new List<GameObject>();
     [SerializeField] List<GameObject> wrongCardsInside = new List<GameObject>();
     [SerializeField] List<GameObject> correctCardsInside = new List<GameObject>();
+    Color green;
+    Color red;
+    Color original;
+
+    private void Start()
+    {
+        ColorUtility.TryParseHtmlString("#1B9738", out green);
+        ColorUtility.TryParseHtmlString("#972F1B", out red);
+        ColorUtility.TryParseHtmlString("#3E455B", out original);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -27,11 +38,18 @@ public class DragInsideMatchDetection : MonoBehaviour
         if (correctCardsInside.Count == 2 && wrongCardsInside.Count == 0)
         {
             Debug.Log("LEVEL COMPLETED");
+            LeanTween.color(GetComponent<Image>().rectTransform, green, .2f);
+        }
+
+        else if (cardsInside.Count >= 2 && wrongCardsInside.Count != 0)
+        {
+            Debug.Log("LEVEL NOT COMPLETED");
+            LeanTween.color(GetComponent<Image>().rectTransform, red, .2f);
         }
 
         else
         {
-            Debug.Log("LEVEL NOT COMPLETED");
+            LeanTween.color(GetComponent<Image>().rectTransform, original, .2f);
         }
 
     }

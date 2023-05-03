c8f1a467b0487e8154f4421edd40e8d9bd39905a
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-05-03 18:31:32 +0300

    Update Tutorial Positioning On Draw Lines Game

--- Assets/Scripts/Games/Draw Lines/TutorialDrawLines.cs
+++ Assets/Scripts/Games/Draw Lines/TutorialDrawLines.cs
@@ -9,6 +9,17 @@ public class TutorialDrawLines : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.Lerp(this.GetComponent<Tutorial>().tutorialPosition.position, targetPosition.position, Mathf.PingPong(Time.time / 2, 1));
+        if(targetPosition.position.y > 0)
+        {
+            transform.position = Vector3.Lerp(new Vector3(this.GetComponent<Tutorial>().tutorialPosition.position.x,
+            (targetPosition.position.y + this.GetComponent<Tutorial>().tutorialPosition.position.y) / 2, 0),
+            targetPosition.position, Mathf.PingPong(Time.time / 2, 1));
+        }
+        else 
+        {
+            transform.position = Vector3.Lerp(new Vector3(this.GetComponent<Tutorial>().tutorialPosition.position.x,
+            targetPosition.position.y, 0),
+            targetPosition.position, Mathf.PingPong(Time.time / 2, 1));
+        }
     }
 }

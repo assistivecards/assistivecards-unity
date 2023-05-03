c66a22ee95115e32779338a8d7b5f4f5fa3408b3
Arda Özkardeşler <aozkardesler@gmail.com>
2023-05-03 15:39:52 +0300

    Generate new board & load 5 unique cards per session

--- Assets/Scripts/Games/Press Cards/PressCardsMatchDetection.cs
+++ Assets/Scripts/Games/Press Cards/PressCardsMatchDetection.cs
@@ -6,12 +6,19 @@ public class PressCardsMatchDetection : MonoBehaviour
 {
     [SerializeField] PressCardsBoardGenerator board;
     [SerializeField] PressCardsCounterSpawner spawner;
+    public int correctMatches;
+    [SerializeField] GameObject cardParent;
 
     public void CheckCount()
     {
         if (board.pressCount == spawner.counter)
         {
-            Debug.Log("LEVEL COMPLETED!");
+            // Debug.Log("LEVEL COMPLETED!");
+            correctMatches++;
+            spawner.enabled = false;
+            board.Invoke("ScaleImagesDown", 1f);
+            board.Invoke("ClearBoard", 1.3f);
+            board.Invoke("GenerateRandomBoardAsync", 1.3f);
         }
     }
 

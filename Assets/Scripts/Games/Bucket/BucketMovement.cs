c0de61a2e94cf65179e94c4041464dd7871f1eda
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-05-08 18:01:13 +0300

    Remove Movement Limits For Diff. Resolutions On Bucket Game

--- Assets/Scripts/Games/Bucket/BucketMovement.cs
+++ Assets/Scripts/Games/Bucket/BucketMovement.cs
@@ -9,24 +9,7 @@ public class BucketMovement : MonoBehaviour, IDragHandler
     public void OnDrag(PointerEventData eventData) 
     {
         var currentPos = transform.position;
-        
-        if(transform.position.x > 300 && transform.position.x < 1780)
-        {
-            transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
-        }
-        else if(transform.position.x <= 300)
-        {
-            if(eventData.delta.x > 0)
-            {
-                transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
-            }
-        }
-        else if(transform.position.x >= 1780)
-        {
-            if(eventData.delta.x < 0)
-            {
-                transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
-            }
-        }
+
+        transform.position = new Vector3(eventData.position.x, currentPos.y, currentPos.z);
     }
 }

21b5d3f35edbeb50a91763e54ff83c10fbaa6550
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-05-11 20:43:18 +0300

    Create Empty Prefab On Game Scene Sorted List

--- Assets/Scripts/Games/Sort Card/SortCardBoardGenerator.cs
+++ Assets/Scripts/Games/Sort Card/SortCardBoardGenerator.cs
@@ -14,14 +14,17 @@ public class SortCardBoardGenerator : MonoBehaviour
     AssistiveCardsSDK.AssistiveCardsSDK.Cards cardDefinitions;
     [SerializeField] AssistiveCardsSDK.AssistiveCardsSDK.Cards cardTextures;
     [SerializeField] private PackSelectionPanel packSelectionPanel;
+    AssistiveCardsSDK.AssistiveCardsSDK.Cards cachedLocalCards;
+    public List<string> cardLocalNames = new List<string>();
+    [SerializeField] AssistiveCardsSDK.AssistiveCardsSDK.Cards cachedCards;
 
     public List<string> cardNames = new List<string>();
     public List<string> cardDefinitionsLocale = new List<string>();
     public List<GameObject> slotableCards  = new List<GameObject>();
-    private string packSlug;
+    public string packSlug;
 
     [SerializeField] private GameObject cardPrefab;
-    private GameObject slotedCardsParent;
+    [SerializeField] private GameObject slotedCardsParent;
     public List<GameObject> slotedCards = new List<GameObject>();
 
     private void Awake()
@@ -32,10 +35,15 @@ public class SortCardBoardGenerator : MonoBehaviour
     public async Task CacheCards(string _packSlug)
     {
         selectedLangCode = await gameAPI.GetSystemLanguageCode();
-        cardDefinitions = await gameAPI.GetCards(selectedLangCode, _packSlug);
-        cardTextures = await gameAPI.GetCards("en", _packSlug);
-        await GenerateRandomBoardAsync(_packSlug);
-        packSlug = _packSlug;
+
+        cachedCards = await gameAPI.GetCards("en", _packSlug);
+        cachedLocalCards = await gameAPI.GetCards(selectedLangCode, _packSlug);
+
+        for(int i = 0; i < cachedCards.cards.Length; i++)
+        {
+            cardNames.Add(cachedCards.cards[i].title.ToLower().Replace(" ", "-"));
+            cardLocalNames.Add(cachedLocalCards.cards[i].title);
+        }
     }
 
     private void GetSlotedList()
@@ -48,26 +56,29 @@ public class SortCardBoardGenerator : MonoBehaviour
 
     public void GeneratStylized()
     {
-        GenerateRandomBoardAsync(packSelectionPanel.selectedPackElement.name);
+        GetSlotedList();
+        packSlug = packSelectionPanel.selectedPackElement.name;
+        GenerateRandomBoardAsync(packSlug);
     }
 
-    public async Task GenerateRandomBoardAsync(string packSlug)
+    public async void GenerateRandomBoardAsync(string packSlug)
     {
         await CacheCards(packSlug);
-        GetSlotedList();
-
-        for(int i = 0; i < slotableCards.Count; i++)
+        for(int i = 0; i < slotedCards.Count; i++)
         {
+
             GameObject card = Instantiate(cardPrefab, slotedCards[i].transform.position, Quaternion.identity);
+            card.transform.SetParent(slotedCards[i].transform);
+            Debug.Log(i);
             
-            int cardImageRandom = Random.Range(0, 2);
-            var cardTexture = await gameAPI.GetCardImage(packSlug, cardNames[cardImageRandom], 512);
+            // int cardImageRandom = Random.Range(0, 2);
+            // var cardTexture = await gameAPI.GetCardImage(packSlug, cardNames[cardImageRandom], 512);
 
-            cardTexture.wrapMode = TextureWrapMode.Clamp;
-            cardTexture.filterMode = FilterMode.Bilinear;
+            // cardTexture.wrapMode = TextureWrapMode.Clamp;
+            // cardTexture.filterMode = FilterMode.Bilinear;
 
-            card.transform.name = cardNames[cardImageRandom];
-            card.transform.GetComponentInChildren<RawImage>().texture = cardTexture;
+            // card.transform.name = cardNames[cardImageRandom];
+            // card.transform.GetComponentInChildren<RawImage>().texture = cardTexture;
         }
     }
 }
--- Assets/Scripts/Games/Sort Card/SortCardUIController.cs
+++ Assets/Scripts/Games/Sort Card/SortCardUIController.cs
@@ -7,12 +7,14 @@ public class SortCardUIController : MonoBehaviour
     [SerializeField] private GameObject gameUI;
     [SerializeField] private GameObject backButton;
     [SerializeField] private GameObject settingButton;
+    [SerializeField] private GameObject helloText;
     
     public void GameUIActivate()
     {
         gameUI.SetActive(true);
         backButton.SetActive(true);
         settingButton.SetActive(true);
+        helloText.SetActive(false);
     }
 
 }

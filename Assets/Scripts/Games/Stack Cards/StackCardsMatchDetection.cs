1e06840cee60810adf2cbdb3b55b4aebaa9df8ae
Arda Özkardeşler <aozkardesler@gmail.com>
2023-05-26 18:38:40 +0300

    Do not allow stacking on draggable cards

--- Assets/Scripts/Games/Stack Cards/StackCardsMatchDetection.cs
+++ Assets/Scripts/Games/Stack Cards/StackCardsMatchDetection.cs
@@ -15,7 +15,7 @@ public class StackCardsMatchDetection : MonoBehaviour, IPointerUpHandler
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.GetChild(0).GetComponent<Image>().sprite == transform.GetChild(0).GetComponent<Image>().sprite && other.transform.childCount == 1)
+        if (other.transform.GetChild(0).GetComponent<Image>().sprite == transform.GetChild(0).GetComponent<Image>().sprite && other.transform.childCount == 1 && other.tag == "FixedCard")
         {
             matchedSlotTransform = other.transform;
             isMatched = true;
@@ -26,7 +26,7 @@ public class StackCardsMatchDetection : MonoBehaviour, IPointerUpHandler
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.transform.GetChild(0).GetComponent<Image>().sprite == transform.GetChild(0).GetComponent<Image>().sprite && other.transform.childCount == 1)
+        if (other.transform.GetChild(0).GetComponent<Image>().sprite == transform.GetChild(0).GetComponent<Image>().sprite && other.transform.childCount == 1 && other.tag == "FixedCard")
         {
             isMatched = false;
             Debug.Log("Correct Match! EXIT");
@@ -42,6 +42,7 @@ public class StackCardsMatchDetection : MonoBehaviour, IPointerUpHandler
             transform.SetParent(matchedSlotTransform);
             LeanTween.moveLocal(gameObject, new Vector3(-20, -20, 0), 0.25f);
             LeanTween.rotate(gameObject, Vector3.zero, .25f);
+            gameObject.tag = "FixedCard";
         }
     }
 

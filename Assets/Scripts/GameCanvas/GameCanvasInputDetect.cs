95a50f12117f16199399df9c8806bba1240ad9bc
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-05-30 14:51:19 +0300

    Create Tutorial Activation Script For No Input

---
+++ Assets/Scripts/GameCanvas/GameCanvasInputDetect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCanvasInputDetect : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorial;   
+    [SerializeField] private GameObject packSelectionPanel;
+    [SerializeField] private GameObject levelChangeScreen;
+    private int time = 0; 
+    void FixedUpdate () 
+    {
+        if(Input.touchCount <= 0 && !packSelectionPanel.activeInHierarchy && !levelChangeScreen.activeInHierarchy)
+        {
+            time = time + 1;
+        } else 
+        {
+            time = 0;
+        }
+        if(time == 800) 
+        {
+            Debug.Log("800 frames passed with no input");
+            tutorial.SetActive(true);
+            time = 0;
+        }
+    }
+}

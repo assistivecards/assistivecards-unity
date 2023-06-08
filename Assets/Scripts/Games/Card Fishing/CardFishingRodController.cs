252fd90af7cd9ea477fb335b39791141000e7d4e
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-06-08 17:06:23 +0300

    Add Rope Stretch Mechanics To Card Fishing

--- Assets/Scripts/Games/Card Fishing/CardFishingRodController.cs
+++ Assets/Scripts/Games/Card Fishing/CardFishingRodController.cs
@@ -1,17 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems; 
 
-public class CardFishingRodController : MonoBehaviour
+public class CardFishingRodController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    [SerializeField] public Transform point1;
-    [SerializeField] public Transform point2;
+    [SerializeField] private GameObject rod;
+    [SerializeField] private Transform point1;
+    [SerializeField] private Transform point2;
+    [SerializeField] private float speed;
+    private bool isPointerDown;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPointerDown = false;
+    }
 
     void Update()
     {
-        if(point1 != null && point2 != null)
+        if(point1 != null && point2 != null && !isPointerDown)
         {
-            transform.position = Vector3.Lerp(point1.position, point2.position, Mathf.PingPong(Time.time, 1));
+            rod.transform.position = Vector3.Lerp(point1.position, point2.position, Mathf.PingPong(Time.time * speed, 1));
+            ResetSize();
         }
+        else if(isPointerDown)
+        {
+            Resize(Time.time * 0.025f);
+        }
+    }
+
+    public void Resize(float resizeAmount)
+    {
+        if(rod.transform.GetChild(1).transform.localScale.y <= 20)
+        {
+            rod.transform.GetChild(1).transform.localScale += new Vector3(0, resizeAmount, 0);
+            rod.transform.GetChild(1).transform.localPosition += new Vector3(0 , -resizeAmount * 10, 0); 
+        }
+    }
+
+    public void ResetSize()
+    {
+        rod.transform.GetChild(1).transform.localScale = new Vector3(rod.transform.GetChild(1).transform.localScale.x, 0, rod.transform.GetChild(1).transform.localScale.z);
+        rod.transform.GetChild(1).transform.localPosition = new Vector3(rod.transform.GetChild(1).transform.localPosition.x, -25f, rod.transform.GetChild(1).transform.localPosition.z);
     }
 }

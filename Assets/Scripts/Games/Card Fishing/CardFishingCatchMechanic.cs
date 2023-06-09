cda1a3f70880ea56dac6e9df727960d82ca9e9ae
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-06-09 18:43:24 +0300

    Add Catch Animation To Card Fishing

--- Assets/Scripts/Games/Card Fishing/CardFishingCatchMechanic.cs
+++ Assets/Scripts/Games/Card Fishing/CardFishingCatchMechanic.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class CardFishingCatchMechanic : MonoBehaviour
 {
     [SerializeField] private CardFishingRodController cardFishingRodController;
-    private bool catctedCard;
-    private GameObject card;
+    public bool catctedCard;
+    public GameObject card;
+    public GameObject formerCard;
 
     private void OnTriggerStay2D(Collider2D other) 
     {
@@ -30,6 +31,35 @@ public class CardFishingCatchMechanic : MonoBehaviour
     private void MoveCard(GameObject _card)
     {
         _card.transform.position = this.transform.GetChild(0).transform.position;
+
+        if(this.transform.localScale.y <= 0)
+        {
+            formerCard = card;
+            card = null;
+            ScaleCard();
+        }
+    }
+
+    private void ScaleCard()
+    {
+        LeanTween.rotateZ(formerCard, 0, 0.2f);
+        LeanTween.scale(formerCard, Vector3.one * 0.6f, 0.5f);
+        Invoke("FadeOutCard", 1f);
+    }
+
+    private void FadeOutCard()
+    {
+        formerCard.GetComponent<Image>().CrossFadeAlpha(0, 0.5f, true);
+        formerCard.transform.GetChild(0).GetComponent<RawImage>().CrossFadeAlpha(0, 0.5f, true);
+        DestroyCard();
+    }
+
+    private void DestroyCard()
+    {
+        Destroy(card);
+        Destroy(formerCard);
+        catctedCard = false;
+        formerCard = null;
     }
 
 }

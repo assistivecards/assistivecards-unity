73c8d39422818524aa00566561f481789b4ef6bd
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-06-14 19:35:39 +0300

    Add Split Mechanic To Card Ninja

--- Assets/Scripts/Games/Card Ninja/CardNinjaCardMovement.cs
+++ Assets/Scripts/Games/Card Ninja/CardNinjaCardMovement.cs
@@ -8,18 +8,23 @@ public class CardNinjaCardMovement : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] private Rigidbody2D cardRB;
 
-    [SerializeField] private Transform horizontalPiecesParent;
-    [SerializeField] private Transform verticalPiecesParent;
-
     [SerializeField] private float maxForce;
     [SerializeField] private float minForce;
 
     [SerializeField] private float lifeTime;
     [SerializeField] private float cardGravityScale;
 
+    private List<Transform> childs = new List<Transform>();
+
+
     private void OnEnable() 
     {
         Invoke("Burst", Random.Range(0f, 65f));
+
+        foreach (Transform child in transform)
+        {
+            childs.Add(child);
+        }
     }
 
     private void Burst() 
@@ -32,6 +37,45 @@ public class CardNinjaCardMovement : MonoBehaviour, IPointerDownHandler
 
     public void OnPointerDown(PointerEventData eventData) 
     {
-        
+        cardRB.simulated = false;
+        for(int i=0; i <= childs.Count; i++)
+        {
+            float childforce = Random.Range(minForce, maxForce);
+
+            // if object cut verticaly
+
+            // if(i == 1 || i==3)
+            // {
+            //     childs[i].GetComponent<Rigidbody2D>().simulated = true;
+            //     childs[i].gameObject.GetComponent<Rigidbody2D>().AddForce(childforce * -transform.right, ForceMode2D.Impulse);
+            //     childs[i].gameObject.GetComponent<Rigidbody2D>().gravityScale = cardGravityScale * 15;
+
+            // }
+            // else if(i == 2 || i==4)
+            // {
+            //     childs[i].GetComponent<Rigidbody2D>().simulated = true;
+            //     childs[i].gameObject.GetComponent<Rigidbody2D>().AddForce(childforce * transform.right, ForceMode2D.Impulse);
+            //     childs[i].gameObject.GetComponent<Rigidbody2D>().gravityScale = cardGravityScale * 15;
+
+            // }
+
+            // if object cut horizontaly
+
+            if(i == 1 || i == 2)
+            {
+                childs[i].GetComponent<Rigidbody2D>().simulated = true;
+                childs[i].gameObject.GetComponent<Rigidbody2D>().AddForce(childforce * -transform.right, ForceMode2D.Impulse);
+                childs[i].gameObject.GetComponent<Rigidbody2D>().gravityScale = cardGravityScale * 15;
+
+            }
+            else if(i == 3 || i==4)
+            {
+                childs[i].GetComponent<Rigidbody2D>().simulated = true;
+                childs[i].gameObject.GetComponent<Rigidbody2D>().AddForce(childforce * transform.right, ForceMode2D.Impulse);
+                childs[i].gameObject.GetComponent<Rigidbody2D>().gravityScale = cardGravityScale * 15;
+
+            }
+            
+        }
     }
 }

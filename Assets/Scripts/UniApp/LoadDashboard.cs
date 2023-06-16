20878f52b02a8b57fea3d33317330b1dfe890302
Arda Özkardeşler <aozkardesler@gmail.com>
2023-06-16 16:00:00 +0300

    Test returning from game scenes to dashboard

---
+++ Assets/Scripts/UniApp/LoadDashboard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadDashboard : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadDashboardScene();
+        }
+
+    }
+
+    public void LoadDashboardScene()
+    {
+        SceneManager.LoadScene("UniApp");
+    }
+}

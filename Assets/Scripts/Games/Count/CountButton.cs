77ce18fdd92a2c7d0f9c27f7dde7775bbf67716e
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-07-06 12:19:13 +0300

    Add Level Ending To Count Game

--- Assets/Scripts/Games/Count/CountButton.cs
+++ Assets/Scripts/Games/Count/CountButton.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountButton : MonoBehaviour
 {
     public int value;
-    public CountGenerateBoard generateBoard;
+    private CountGenerateBoard generateBoard;
+    private CountUIController uıController;
 
     private void OnEnable() 
     {
         generateBoard = FindObjectOfType<CountGenerateBoard>();
+        uıController = FindObjectOfType<CountUIController>();
     }
 
 
@@ -17,7 +20,22 @@ public class CountButton : MonoBehaviour
     {
         if(value == generateBoard.countNum + 1)
         {
-            Debug.Log("TRUE MATCH");
+            Invoke("CallLevelEnd", 0.25f);
         }
+        else
+        {
+            LeanTween.scale(this.gameObject, Vector3.one * 1.5f, 0.25f).setOnComplete(ScaleDown);
+        }
+    }
+
+    private void CallLevelEnd()
+    {
+        generateBoard.ClearBoard();
+        uıController.LevelChangeScreenActivate();
+    }
+
+    private void ScaleDown()
+    {
+        LeanTween.scale(this.gameObject, Vector3.zero, 0.20f);
     }
 }

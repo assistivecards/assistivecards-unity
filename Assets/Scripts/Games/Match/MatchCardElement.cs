80c6439b0c7362c04db9e38ec1fea2ebd3426e13
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-07-11 14:23:06 +0300

    Create Match Detection For Match Game

--- Assets/Scripts/Games/Match/MatchCardElement.cs
+++ Assets/Scripts/Games/Match/MatchCardElement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MatchCardElement : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class MatchCardElement : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     GameAPI gameAPI;
 
@@ -12,9 +12,9 @@ public class MatchCardElement : MonoBehaviour, IPointerDownHandler, IDragHandler
     public bool match;
     private GameObject levelChange;
     private LevelChangeScreenHatchMatch levelChangeScreenHatchMatch;
-
     public string cardName;
     private Vector3 startPosition;
+    private bool isPointerUp;
 
     private void Awake() 
     {
@@ -24,28 +24,60 @@ public class MatchCardElement : MonoBehaviour, IPointerDownHandler, IDragHandler
 
     public void OnDrag(PointerEventData eventData)
     {
-        if(moveable)
+        if(!match && moveable)
             this.transform.position = eventData.position;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        isPointerUp = false;
         if(!match && moveable)
             this.transform.position = eventData.position;
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPointerUp = true;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
     {   
+        if(isPointerUp)
+        {
+            if(!match 
+            && other.gameObject.GetComponent<MatchCardElement>().moveable == false  
+            && other.gameObject.GetComponent<MatchCardElement>().cardName == cardName)
+            {
+                LeanTween.move(this.gameObject, other.transform.position, 0.25f);
+                other.gameObject.GetComponent<MatchCardElement>().match = true;
+                match = true;
+                SpeakCardName();
+            }
+            else if(!match 
+            && other.gameObject.GetComponent<MatchCardElement>().moveable == false 
+            && other.gameObject.GetComponent<MatchCardElement>().cardName != cardName)
+            {
+                moveable = false;
+                Invoke("MoveToBegging", 0.25f);
+            }
+        }
 
     }
 
     private void MoveToBegging()
     {
-        LeanTween.move(this.gameObject, startPosition, 1.25f);
+        LeanTween.move(this.gameObject, startPosition, 1.25f).setOnComplete(SetMoveableTrue);
+        Debug.Log("MoveToBegging");
     }
 
     private void SpeakCardName()
     {
         gameAPI.Speak(cardName);
+        Debug.Log(cardName);
+    }
+
+    private void SetMoveableTrue()
+    {
+        moveable = true;
     }
 }

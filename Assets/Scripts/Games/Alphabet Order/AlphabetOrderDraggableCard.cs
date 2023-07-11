06c128596bee6a6cd371043fc0bb7ac79d22d697
Arda Özkardeşler <aozkardesler@gmail.com>
2023-07-11 10:45:20 +0300

    Add draggable cards

---
+++ Assets/Scripts/Games/Alphabet Order/AlphabetOrderDraggableCard.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AlphabetOrderDraggableCard : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = transform.position + new Vector3(eventData.delta.x, eventData.delta.y, 0);
+        transform.SetParent(GameObject.Find("GamePanel").transform);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("PointerDown");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("PointerUp");
+    }
+
+}

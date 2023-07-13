26afdcbfde06835eef89f429bc79157faad52150
Arda Özkardeşler <aozkardesler@gmail.com>
2023-07-13 16:19:10 +0300

    Do not allow multiple dragging in Alphabet Order

--- Assets/Scripts/Games/Alphabet Order/AlphabetOrderDraggableCard.cs
+++ Assets/Scripts/Games/Alphabet Order/AlphabetOrderDraggableCard.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class AlphabetOrderDraggableCard : MonoBehaviour, IDragHandler, IPointerDownHandler
+public class AlphabetOrderDraggableCard : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
     private GameAPI gameAPI;
     public string parentName;
+    private AlphabetOrderBoardGenerator board;
 
     private void Awake()
     {
         gameAPI = Camera.main.GetComponent<GameAPI>();
+    }
+
+    private void Start()
+    {
+        board = GameObject.Find("GamePanel").GetComponent<AlphabetOrderBoardGenerator>();
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -23,6 +29,24 @@ public class AlphabetOrderDraggableCard : MonoBehaviour, IDragHandler, IPointerD
         gameAPI.VibrateWeak();
         gameAPI.PlaySFX("Pickup");
         parentName = transform.parent.name;
+
+        for (int i = 0; i < board.cardParents.Length; i++)
+        {
+            board.cardParents[i].GetComponent<AlphabetOrderDraggableCard>().enabled = false;
+        }
+        gameObject.GetComponent<AlphabetOrderDraggableCard>().enabled = true;
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        for (int i = 0; i < board.cardParents.Length; i++)
+        {
+            if (!board.cardParents[i].GetComponent<AlphabetOrderMatchDetection>().isMatched)
+            {
+                board.cardParents[i].GetComponent<AlphabetOrderDraggableCard>().enabled = true;
+            }
+        }
     }
 
 }

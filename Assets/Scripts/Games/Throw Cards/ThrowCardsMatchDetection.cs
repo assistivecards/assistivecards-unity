caab485049eb5b6b4885b14b60bb929033aaf8c4
Arda Özkardeşler <aozkardesler@gmail.com>
2023-07-24 16:45:00 +0300

    Add Checkpoint Panel

--- Assets/Scripts/Games/Throw Cards/ThrowCardsMatchDetection.cs
+++ Assets/Scripts/Games/Throw Cards/ThrowCardsMatchDetection.cs
@@ -7,17 +7,20 @@ public class ThrowCardsMatchDetection : MonoBehaviour
 {
     private Rigidbody2D rb;
     private ThrowCardsBoardGenerator board;
+    private ThrowCardsUIController UIController;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         board = GameObject.Find("GamePanel").GetComponent<ThrowCardsBoardGenerator>();
+        UIController = GameObject.Find("GamePanel").GetComponent<ThrowCardsUIController>();
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.GetChild(0).GetComponent<Image>().sprite == transform.GetChild(0).GetComponent<SpriteRenderer>().sprite)
         {
             Debug.Log("CORRECT MATCH");
+            UIController.correctMatches++;
             rb.sharedMaterial.bounciness = 0;
             rb.velocity = Vector2.zero;
             rb.gravityScale = 0;
@@ -26,7 +29,13 @@ public class ThrowCardsMatchDetection : MonoBehaviour
             Invoke("PlayCorrectMatchAnimation", 0.25f);
             board.Invoke("ScaleImagesDown", 1f);
             board.Invoke("ClearBoard", 1.3f);
-            board.Invoke("GenerateRandomBoardAsync", 1.3f);
+
+            if (UIController.correctMatches == UIController.checkpointFrequency)
+            {
+                UIController.Invoke("OpenCheckPointPanel", 1.3f);
+            }
+            else
+                board.Invoke("GenerateRandomBoardAsync", 1.3f);
 
         }
 

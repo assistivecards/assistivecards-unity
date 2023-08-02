807066c6bb552f7c4e729655c480725dbd0b331d
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-08-02 12:14:38 +0300

    Create Pattern Cards On Pattern Train

--- Assets/Scripts/Games/Pattern Train/PatternTrainBoardGenerator.cs
+++ Assets/Scripts/Games/Pattern Train/PatternTrainBoardGenerator.cs
@@ -36,23 +36,13 @@ public class PatternTrainBoardGenerator : MonoBehaviour
     [SerializeField] private GameObject cardPrefab;
     [SerializeField] private GameObject tutorial;
 
-    [Header ("Game UI")]
-    public GameObject cardPosition;
-    public GameObject firstLetterText;
-    private List<GameObject> buttons = new List<GameObject>();
+    [Header ("Pattern Positions")]
+    [SerializeField] private Transform patternPositionsParent;
+    private List<GameObject> patternPositions = new List<GameObject>();
 
-    [Header ("Colors")]
-    public Color[] colors;
-
-    public int levelCount;
-    private string cardName;
-    private GameObject letterCard;
-    private GameObject correctButton;
-    private int random;
-    public int cardNameLenght;
-    public string firstLetter;
-    public string formerLetter;
-
+    [Header ("Draggable Positions")]
+    [SerializeField] private Transform draggablePositionsParent;
+    private List<GameObject> draggablePositions = new List<GameObject>();
     private void Awake()
     {
         gameAPI = Camera.main.GetComponent<GameAPI>();
@@ -91,7 +81,14 @@ public class PatternTrainBoardGenerator : MonoBehaviour
 
     private void CreatePositionsList()
     {
-
+        foreach(Transform child in patternPositionsParent)
+        {
+            patternPositions.Add(child.gameObject);
+        }
+        foreach(Transform child in draggablePositionsParent)
+        {
+            draggablePositions.Add(child.gameObject);
+        }
     }
 
     public async void GeneratedBoardAsync()
@@ -99,117 +96,29 @@ public class PatternTrainBoardGenerator : MonoBehaviour
         //if(uıController.canGenerate)
         //{
             await CacheCards();
+            CreatePositionsList();
             for(int i = 0; i < 3; i++)
             {
                 CheckRandom();
+                GameObject card = Instantiate(cardPrefab, patternPositions[i].transform.position, Quaternion.identity);
+                card.transform.SetParent( patternPositions[i].transform);
+
                 var cardTexture = await gameAPI.GetCardImage(packSelectionPanel.selectedPackElement.name, cardNames[randomValueList[i]], 512);
                 cardTexture.wrapMode = TextureWrapMode.Clamp;
                 cardTexture.filterMode = FilterMode.Bilinear;
 
-                buttons[i].transform.name = cardLocalNames[randomValueList[i]];
-                buttons[i].transform.GetChild(0).GetComponent<RawImage>().texture = cardTexture;
-                buttons[i].transform.GetChild(0).GetComponent<RawImage>().color = new Color(255, 255, 255, 255);
-                cards.Add(buttons[i]);
+                card.transform.name = cardLocalNames[randomValueList[i]];
+                card.transform.GetChild(0).GetComponent<RawImage>().texture = cardTexture;
+                card.transform.GetChild(0).GetComponent<RawImage>().color = new Color(255, 255, 255, 255);
+                cards.Add(card);
             }
         //}
-        formerLetter = firstLetter;
-        FillLetterCard();
         Invoke("GameUIActivate", 0.3f);
-    }
-
-    private async void FillLetterCard()
-    {
-        random = Random.Range(0,3);
-        GetFirstLetter(cards[random]);
-        if(formerLetter == firstLetter)
-        {
-            random = Random.Range(0,3);
-            GetFirstLetter(cards[random]);
-        }
-        correctButton = cards[random];
-        letterCard = Instantiate(cardPrefab, cardPosition.transform.position, Quaternion.identity);
-        letterCard.transform.SetParent(cardPosition.transform);
-
-        if(letterCardsNames.Contains(firstLetter))
-        {
-            foreach(var letter in letterCardsNames)
-            {
-                if(firstLetter == letter.Substring(0, 1))
-                {
-                    letterCard.transform.GetChild(0).gameObject.SetActive(true);
-                    letterCard.transform.GetChild(1).gameObject.SetActive(false);
-
-                    var correctLetterTexture = await gameAPI.GetCardImage("letters", letter, 512);
-                    correctLetterTexture.wrapMode = TextureWrapMode.Clamp;
-                    correctLetterTexture.filterMode = FilterMode.Bilinear;
-
-                    letterCard.transform.GetChild(0).transform.GetComponent<RawImage>().texture = correctLetterTexture;   
-                    LeanTween.scale(letterCard.gameObject, Vector3.one * 0.5f, 0f);
-                }
-            }
-        }
-        else
-        {
-            letterCard.transform.GetChild(0).gameObject.SetActive(false);
-            letterCard.transform.GetChild(1).gameObject.SetActive(true);
-            letterCard.transform.GetChild(1).GetComponent<TMP_Text>().text = firstLetter.ToLower();
-            letterCard.transform.GetChild(1).GetComponent<TMP_Text>().color = colors[Random.Range(0, colors.Length)];
-        }
-        firstLetterText.GetComponent<TMP_Text>().text = gameAPI.Translate(firstLetterText.gameObject.name, gameAPI.ToSentenceCase(firstLetter).Replace("-", " "), selectedLangCode);
-    }
-
-    private void GetFirstLetter(GameObject _card)
-    {
-        cardName = _card.name;
-        firstLetter = cardName.Substring(0, 1).ToLower();
-        cardNameLenght = _card.name.Length;
     }
 
     public void GameUIActivate()
     {
         //uıController.GameUIActivate();
-
-        buttons[0].transform.localPosition = new Vector3(-300, -190, 0);
-        buttons[1].transform.localPosition = new Vector3(0, -190, 0);
-        buttons[2].transform.localPosition = new Vector3(300, -190, 0);
-
-        foreach(GameObject button in buttons)
-        {
-            LeanTween.scale(button, Vector3.one, 0.1f);
-        }
-
-
-        LeanTween.scale(firstLetterText, Vector3.one, 0.1f);
-        //tutorial.GetComponent<AlphabetChooseTutorial>().SetPosition(cards[random].transform);
-
-    }
-
-    public void LevelEnding()
-    {
-        LeanTween.moveLocal(correctButton, Vector3.zero, 0.2f).setOnComplete(ScaleUpCard);
-        foreach(var button in buttons)
-        {
-            if(button != correctButton)
-                LeanTween.scale(button, Vector3.zero, 0.1f);
-        }
-        LeanTween.scale(letterCard, Vector3.zero, 0.1f);
-        LeanTween.scale(firstLetterText, Vector3.zero, 0.1f);
-    }
-
-    private void ScaleUpCard()
-    {
-        LeanTween.scale(correctButton, Vector3.one * 1.5f, 0.5f).setOnComplete(ScaleDownCard);
-    }
-
-    private void ScaleDownCard()
-    {
-        LeanTween.scale(correctButton, Vector3.zero, 0.4f);
-    }
-
-    private void CreateNewLevel()
-    {
-        ClearBoard();
-        GeneratedBoardAsync();
     }
 
     public void ClearBoard()
@@ -222,20 +131,5 @@ public class PatternTrainBoardGenerator : MonoBehaviour
         letterCardsNames.Clear();
 
         randomValueList.Clear();
-
-        foreach(GameObject button in buttons)
-        {
-            LeanTween.scale(button, Vector3.zero, 0.1f);
-            button.name = "Button";
-
-            if (EventSystem.current.currentSelectedGameObject == button)
-            {
-                EventSystem.current.SetSelectedGameObject(null);
-            }
-        }
-        buttons.Clear();
-
-        Destroy(letterCard);
-
     }
 }

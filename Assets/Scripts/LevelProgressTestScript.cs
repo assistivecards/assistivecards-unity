a6229e03d563c13ce3db59477e12b22152d9a6a8
Arda Özkardeşler <aozkardesler@gmail.com>
2023-08-02 18:12:07 +0300

    Update level progress bar fill percentage

---
+++ Assets/Scripts/LevelProgressTestScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LevelProgressTestScript : MonoBehaviour
+{
+    private GameAPI gameAPI;
+    [SerializeField] TMP_Text levelText;
+    [SerializeField] Slider progressBar;
+
+    private void Awake()
+    {
+        gameAPI = Camera.main.GetComponent<GameAPI>();
+        gameAPI.SetExp(5);
+    }
+    private void OnEnable()
+    {
+        var level = gameAPI.CalculateLevel(gameAPI.GetExp());
+        var minExp = gameAPI.CalculateExp(level);
+        var maxExp = gameAPI.CalculateExp(level + 1);
+        levelText.text = "Level " + level;
+
+        progressBar.minValue = minExp;
+        progressBar.maxValue = maxExp;
+        progressBar.value = gameAPI.GetExp();
+
+    }
+
+}

d53998deb03d88813df72e2338fe753b8532165c
Arda Özkardeşler <aozkardesler@gmail.com>
2023-08-08 11:30:08 +0300

    Add session exp system to Press Cards

--- Assets/Scripts/Games/Press Cards/PressCardsUIController.cs
+++ Assets/Scripts/Games/Press Cards/PressCardsUIController.cs
@@ -54,6 +54,7 @@ public class PressCardsUIController : MonoBehaviour
 
     IEnumerator CloseCheckPointPanelCoroutine()
     {
+        gameAPI.ResetSessionExp();
         LeanTween.scale(checkPointPanel, Vector3.zero, 0.25f);
         yield return new WaitForSeconds(0.5f);
         checkPointPanel.SetActive(false);
@@ -67,6 +68,7 @@ public class PressCardsUIController : MonoBehaviour
 
     IEnumerator ChooseNewPackButtonCoroutine()
     {
+        gameAPI.ResetSessionExp();
         board.ScaleImagesDown();
         backButton.SetActive(false);
         CloseCheckpointPanel();
@@ -118,6 +120,7 @@ public class PressCardsUIController : MonoBehaviour
     IEnumerator BackButtonClickCoroutine()
     {
         ResetCounter();
+        gameAPI.ResetSessionExp();
         // board.ClearUniqueCards();
         board.ScaleImagesDown();
         backButton.SetActive(false);

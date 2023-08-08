ab3b91d1f9dd03dcd6baea190a60640f980de3ea
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-08-08 18:33:52 +0300

    Add Snake Rotation Movement To Snake Cards

--- Assets/Scripts/Games/Snake Cards/SnakeCardTrailMove.cs
+++ Assets/Scripts/Games/Snake Cards/SnakeCardTrailMove.cs
@@ -7,16 +7,21 @@ using TMPro;
 public class SnakeCardTrailMove : MonoBehaviour
 {
     [SerializeField] private GameObject snake;
+    [SerializeField] private float speed;
     public Vector2 firstTouchPosition;
     public Vector2 secondTouchPosition;
     public Vector2 currentSwipe;
     public Vector2 direction;
+    public bool move;
+    private int degree;
 
     private void Update() 
     {
-        snake.transform.position += transform.right * Time.deltaTime * 0.5f;
-
-        if (Input.touchCount > 0)
+        if(degree == 90) { snake.transform.position += transform.right * Time.deltaTime * speed;}
+        else if(degree == -90) { snake.transform.position += -transform.right * Time.deltaTime * speed;}
+        else if(degree == 180) { snake.transform.position += transform.up * Time.deltaTime * speed;}
+        else if(degree == 0) { snake.transform.position += -transform.up * Time.deltaTime * speed;}
+        if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             switch (touch.phase)
@@ -34,6 +39,47 @@ public class SnakeCardTrailMove : MonoBehaviour
         }
     }
 
+    private void CheckSnakeIfInScreen()
+    {
+        if(snake.transform.localPosition.x > -500 && snake.transform.localPosition.x < 500 
+        && snake.transform.localPosition.y < 300 && snake.transform.localPosition.y > -250)
+        {
+            move = true;
+        }
+        else if(snake.transform.position.x < -500)
+        {
+            move = false;
+            if(snake.transform.position.y > 0) { RotateSnake(0);}
+            if(snake.transform.position.y <= 0) { RotateSnake(180);}
+            snake.transform.position = new Vector3(-499, snake.transform.position.y, 0);
+            move = true;
+        }
+        else if(snake.transform.position.x > 500)
+        {
+            move = false;
+            if(snake.transform.position.y > 0) { RotateSnake(0);}
+            if(snake.transform.position.y <= 0) { RotateSnake(180);}
+            snake.transform.position = new Vector3(499, snake.transform.position.y, 0);
+            move = true;
+        }
+        else if(snake.transform.position.y > 300)
+        {
+            move = false;
+            if(snake.transform.position.x > 0) { RotateSnake(-90);}
+            if(snake.transform.position.x <= 0) { RotateSnake(90);}
+            snake.transform.position = new Vector3(299, snake.transform.position.y, 0);
+            move = true;
+        }
+        else if(snake.transform.position.y < -250)
+        {
+            move = false;
+            if(snake.transform.position.x > 0) { RotateSnake(-90);}
+            if(snake.transform.position.x <= 0) { RotateSnake(90);}
+            snake.transform.position = new Vector3(-249, snake.transform.position.y, 0);
+            move = true;
+        }
+    }
+
     private void DetectDirection()
     {
         direction = secondTouchPosition - firstTouchPosition;
@@ -41,17 +87,17 @@ public class SnakeCardTrailMove : MonoBehaviour
         if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && direction.x > 0)
         {
             Debug.Log("right");
-            RotateSnake(0);
+            RotateSnake(90);
         }
         else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y) && direction.y > 0)
         {
             Debug.Log("up");
-            RotateSnake(0);
+            RotateSnake(180);
         }
         else if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && direction.x < 0)
         {
             Debug.Log("left");
-            RotateSnake(0);
+            RotateSnake(-90);
         }
         else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y) && direction.y < 0)
         {
@@ -60,8 +106,10 @@ public class SnakeCardTrailMove : MonoBehaviour
         }
     }
 
-    private void RotateSnake(int degree)
+    private void RotateSnake(int _degree)
     {
+        LeanTween.rotate(snake, new Vector3(0,0, _degree), 0.1f);
+        degree = _degree;
         direction = Vector2.zero;
     }
 }

5a69223b0739a95690d92dd4cd6480e1ea28e89d
Arda Özkardeşler <aozkardesler@gmail.com>
2023-08-25 18:17:18 +0300

    Clear unique cards on sign out
    * Fix unique cards list not updating on language change

--- Assets/Scripts/Games/Card Maze/CardMazeBoardGenerator.cs
+++ Assets/Scripts/Games/Card Maze/CardMazeBoardGenerator.cs
@@ -62,6 +62,10 @@ public class CardMazeBoardGenerator : MonoBehaviour
         if (didLanguageChange)
         {
             await CacheCards(packSlug);
+            for (int i = 0; i < uniqueCards.Count; i++)
+            {
+                uniqueCards[i] = cachedCards.cards.Where(card => card.slug == uniqueCards[i].slug).ToList()[0];
+            }
             didLanguageChange = false;
         }
 

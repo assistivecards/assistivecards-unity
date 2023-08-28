fd9949c40062efe61e28e16b4b49a1f1f2674938
Arda Özkardeşler <aozkardesler@gmail.com>
2023-08-28 18:57:48 +0300

    Fix tutorial animation for flipped mazes

--- Assets/Scripts/Games/Card Maze/CardMazeTutorial.cs
+++ Assets/Scripts/Games/Card Maze/CardMazeTutorial.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class CardMazeTutorial : MonoBehaviour
 {
     [SerializeField] Vector3 startPos;
-    [SerializeField] Vector3 endPos;
+    [SerializeField] Vector3 target;
     [SerializeField] CardMazeBoardGenerator board;
 
     private void OnEnable()
     {
         startPos = board.cardParent.transform.localPosition;
-        endPos = GameObject.Find("TutorialFinishTarget").transform.localPosition;
+        target = GameObject.Find("TutorialFinishTarget").transform.localPosition;
+
+        if (board.isFlipped)
+        {
+            target = new Vector3(-target.x, target.y, target.z);
+        }
+
 
     }
 
     void Update()
     {
-        if (startPos != null && endPos != null)
+        if (startPos != null && target != null)
         {
-            transform.localPosition = Vector3.Lerp(startPos, endPos, Mathf.PingPong(Time.time, 1));
+            transform.localPosition = Vector3.Lerp(startPos, target, Mathf.PingPong(Time.time, 1));
         }
     }
 }

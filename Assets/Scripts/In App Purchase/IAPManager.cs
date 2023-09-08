838351073ad2d8b005648350dd6e0dd8a65ac6f2
Arda Özkardeşler <aozkardesler@gmail.com>
2023-09-08 17:22:47 +0300

    Fix IAP prices not being displayed initially in UniApp

--- Assets/Scripts/In App Purchase/IAPManager.cs
+++ Assets/Scripts/In App Purchase/IAPManager.cs
@@ -208,18 +208,18 @@ public class IAPManager : MonoBehaviour, IDetailedStoreListener
 
         IAPUIManager.CheckIfPremiumButtonInteractable();
 
-        // subscriptionsScreenPrice.text = IAPManager.m_StoreController.products.WithID(premium).metadata.localizedPriceString;
+        subscriptionsScreenPrice.text = IAPManager.m_StoreController.products.WithID(premium).metadata.localizedPriceString;
 
-        // promoScreenPrice.text = IAPManager.m_StoreController.products.WithID(premium).metadata.localizedPriceString;
-        // promoScreenPuchasePrice.text = IAPManager.m_StoreController.products.WithID(premium).metadata.localizedPriceString;
+        promoScreenPrice.text = IAPManager.m_StoreController.products.WithID(premium).metadata.localizedPriceString;
+        promoScreenPuchasePrice.text = IAPManager.m_StoreController.products.WithID(premium).metadata.localizedPriceString;
 
-        // if (subscriptionsScreenMonthlyPrice != null)
-        // {
-        //     subscriptionsScreenMonthlyPrice.text = IAPManager.m_StoreController.products.WithID(monthly).metadata.localizedPriceString;
-        //     subscriptionsScreenYearlyPrice.text = IAPManager.m_StoreController.products.WithID(yearly).metadata.localizedPriceString;
-        //     promoScreenMonthlyPrice.text = IAPManager.m_StoreController.products.WithID(monthly).metadata.localizedPriceString;
-        //     promoScreenYearlyPrice.text = IAPManager.m_StoreController.products.WithID(yearly).metadata.localizedPriceString;
-        // }
+        if (subscriptionsScreenMonthlyPrice != null)
+        {
+            subscriptionsScreenMonthlyPrice.text = IAPManager.m_StoreController.products.WithID(monthly).metadata.localizedPriceString;
+            subscriptionsScreenYearlyPrice.text = IAPManager.m_StoreController.products.WithID(yearly).metadata.localizedPriceString;
+            promoScreenMonthlyPrice.text = IAPManager.m_StoreController.products.WithID(monthly).metadata.localizedPriceString;
+            promoScreenYearlyPrice.text = IAPManager.m_StoreController.products.WithID(yearly).metadata.localizedPriceString;
+        }
 
     }
 

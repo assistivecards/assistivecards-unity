0f9188533d2e144d8918e0a0dea875ec70984212
Arda Özkardeşler <aozkardesler@gmail.com>
2023-09-19 16:28:20 +0300

    Do not allow multi-touch in Finger Paint

--- Assets/Scripts/Games/Finger Paint/PaintImage.cs
+++ Assets/Scripts/Games/Finger Paint/PaintImage.cs
@@ -132,13 +132,17 @@ public class PaintImage : MonoBehaviour, IPointerEnterHandler
     {
 
         // Vector2 localPt;
-        Vector2 mPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, mPos, uiCamera, out localPt);
-
-        if (gameObject.GetComponent<RectTransform>().rect.Contains((localPt)))
+        if (Input.touchCount == 1)
         {
-            CheckInput();
+            Vector2 mPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, mPos, uiCamera, out localPt);
+
+            if (gameObject.GetComponent<RectTransform>().rect.Contains((localPt)))
+            {
+                CheckInput();
+            }
         }
+
 
     }
 

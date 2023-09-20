513c520b68a12d3560c9bd444c96875abae3a4e2
Arda Özkardeşler <aozkardesler@gmail.com>
2023-09-20 17:48:16 +0300

    Do not render cut trail on multi-touch in Rope Cut

--- Assets/Scripts/Games/Rope Cut/TrailManager.cs
+++ Assets/Scripts/Games/Rope Cut/TrailManager.cs
@@ -17,21 +17,25 @@ public class TrailManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0)))
+        if (Input.touchCount == 1)
         {
-            gameObject.GetComponent<TrailRenderer>().enabled = true;
-            Plane objPlane = new Plane(Camera.main.transform.forward * -1, new Vector3(transform.position.x, transform.position.y, transform.position.z));
+            if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0)))
+            {
+                gameObject.GetComponent<TrailRenderer>().enabled = true;
+                Plane objPlane = new Plane(Camera.main.transform.forward * -1, new Vector3(transform.position.x, transform.position.y, transform.position.z));
 
-            Ray mRay = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y));
+                Ray mRay = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y));
 
-            float rayDistance;
-            if (objPlane.Raycast(mRay, out rayDistance))
-                transform.position = mRay.GetPoint(rayDistance);
+                float rayDistance;
+                if (objPlane.Raycast(mRay, out rayDistance))
+                    transform.position = mRay.GetPoint(rayDistance);
+            }
+            else
+            {
+                gameObject.GetComponent<TrailRenderer>().enabled = false;
+            }
         }
-        else
-        {
-            gameObject.GetComponent<TrailRenderer>().enabled = false;
-        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)

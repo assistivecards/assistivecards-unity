44f3986bcbf55c26cf0883c60331569dbf37915e
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-09-25 17:02:17 +0300

    Add Swap Mechanic To Swap Cards

--- Assets/Scripts/Games/Swap Cards/SwapCardsCardController.cs
+++ Assets/Scripts/Games/Swap Cards/SwapCardsCardController.cs
@@ -6,23 +6,34 @@ using UnityEngine.EventSystems;
 public class SwapCardsCardController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     private GameAPI gameAPI;
+    private bool isPointerUp;
 
     private void Awake()
     {
         gameAPI = Camera.main.GetComponent<GameAPI>();
     }
     
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void OnTriggerStay2D(Collider2D other) 
     {
-        if(other.transform.tag == "Border")
+        if(other.transform.tag == "Border" && this.transform.parent.gameObject != other.gameObject)
         {
-            Debug.Log("Border Detection");
+            if(isPointerUp)
+            {
+                GameObject otherCard = other.transform.GetChild(0).gameObject;
+                Transform thisCardParent = this.transform.parent.transform;
+
+                LeanTween.move(this.gameObject, other.transform.position, 0.25f);
+                LeanTween.move(otherCard, thisCardParent.position, 0.25f);
+
+                otherCard.transform.parent = thisCardParent;
+                this.transform.parent = other.transform;
+            }
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if(Input.touchCount == 1)
         {
             transform.position = transform.position + new Vector3(eventData.delta.x, eventData.delta.y, 0);
         }
@@ -30,18 +41,19 @@ public class SwapCardsCardController : MonoBehaviour, IPointerDownHandler, IPoin
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if(Input.touchCount == 1)
         {
-            gameAPI.VibrateWeak();
             transform.GetComponent<Rigidbody2D>().isKinematic = true;
+            isPointerUp = false;
         }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if(Input.touchCount == 1)
         {
             transform.GetComponent<Rigidbody2D>().isKinematic = false;
+            isPointerUp = true;
         }
     }
 }

3e21f126dfed061793d2e36c7ae052d2026e5f8d
Arda Özkardeşler <aozkardesler@gmail.com>
2023-09-25 15:38:47 +0300

    Fix multiple balloon pops

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Balloon : MonoBehaviour, IPointerClickHandler
 {
@@ -15,6 +16,7 @@ public class Balloon : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         gameObject.GetComponent<Animator>().enabled = true;
+        gameObject.GetComponent<Image>().raycastTarget = false;
         gameAPI.PlaySFX("Pop");
         gameAPI.VibrateWeak();
         Destroy(gameObject, .5f);

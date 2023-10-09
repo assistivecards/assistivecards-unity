3c138571dd52685c014f5893e1c0df82cf0bcea2
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-10-09 16:09:51 +0300

    Add Reset Direction To Needle Thread

--- Assets/Scripts/Games/Needle Thread/NeedleThreadDetectRotation.cs
+++ Assets/Scripts/Games/Needle Thread/NeedleThreadDetectRotation.cs
@@ -27,31 +27,44 @@ public class NeedleThreadDetectRotation : MonoBehaviour
                     break;
 
                 case TouchPhase.Ended:
-                    direction = Vector3.zero;
+                    ResetMovement();
                     break;
             }
         }
         DetectDirection();
+        if(direction.x > 100 || direction.x < 100  || direction.y > 100 || direction.y < 100)
+        {
+            ResetMovement();
+        }
     }
 
     private void DetectDirection()
     {
         direction = secondTouchPosition - firstTouchPosition;
-        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && direction.x > 0 && directionStatus != "down") 
+        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && direction.x > 0) 
         {
             LeanTween.rotate(needle, new Vector3(0, 0, -90), 0.1f);
+            //up
         }
-        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y) && direction.y > 0 && directionStatus != "left") 
+        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y) && direction.y > 0) 
         {
             LeanTween.rotate(needle, new Vector3(0, 0, 0), 0.1f);
+            //right
         }
-        else if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && direction.x < 0 && directionStatus != "up") 
+        else if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && direction.x < 0) 
         {
             LeanTween.rotate(needle, new Vector3(0, 0, 90), 0.1f);
+            // down
         }
-        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y) && direction.y < 0 && directionStatus != "right") 
+        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y) && direction.y < 0) 
         {
             LeanTween.rotate(needle, new Vector3(0, 0, 180), 0.1f);
+            //left
         }
+    }
+
+    private void ResetMovement()
+    {
+        direction = Vector3.zero;
     }
 }

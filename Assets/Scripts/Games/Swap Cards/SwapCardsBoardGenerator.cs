f5e97f36599c402865d7ea988ae878bc265f1cf7
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-10-26 18:48:01 +0300

    Change Tutorial To Per Session On Swap Cards

--- Assets/Scripts/Games/Swap Cards/SwapCardsBoardGenerator.cs
+++ Assets/Scripts/Games/Swap Cards/SwapCardsBoardGenerator.cs
@@ -436,5 +436,6 @@ public class SwapCardsBoardGenerator : MonoBehaviour
             Debug.Log(cardNames[randomValueList[i]]);
         }
         Invoke("GeneratedBoardAsync", 2f);
+        uıController.Invoke("SetTutorialActive", 2.1f);
     }
 }

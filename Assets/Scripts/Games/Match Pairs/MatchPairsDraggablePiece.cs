db4a145e37f85f81153be65625e4e6a4a97b5ebe
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-12-26 15:54:55 +0300

    Bugfix Touch Input Match Pairs

--- Assets/Scripts/Games/Match Pairs/MatchPairsDraggablePiece.cs
+++ Assets/Scripts/Games/Match Pairs/MatchPairsDraggablePiece.cs
@@ -14,27 +14,50 @@ public class MatchPairsDraggablePiece : MonoBehaviour, IPointerDownHandler, IPoi
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             transform.position = transform.position + new Vector3(eventData.delta.x, eventData.delta.y, 0);
+        }
+        else
+        {
+            if (Input.touchCount == 1)
+            {
+                transform.position = transform.position + new Vector3(eventData.delta.x, eventData.delta.y, 0);
+            }
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             gameAPI.VibrateWeak();
             gameAPI.PlaySFX("Pickup");
             transform.GetComponent<Rigidbody2D>().isKinematic = true;
         }
+        else
+        {
+            if (Input.touchCount == 1)
+            {
+                gameAPI.VibrateWeak();
+                gameAPI.PlaySFX("Pickup");
+                transform.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             transform.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+        else
+        {
+            if (Input.touchCount == 1)
+            {
+                transform.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
         }
     }
 }

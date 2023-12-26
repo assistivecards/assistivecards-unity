fafe2ed638f40b77e092918bcc36574e290512ec
Özlem <88405137+ogucukbel@users.noreply.github.com>
2023-12-26 10:35:04 +0300

    Bugfix Touch Input Stack Cards

--- Assets/Scripts/Games/Stack Cards/StackCardsDraggableCards.cs
+++ Assets/Scripts/Games/Stack Cards/StackCardsDraggableCards.cs
@@ -14,25 +14,43 @@ public class StackCardsDraggableCards : MonoBehaviour, IDragHandler, IPointerDow
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             transform.position = transform.position + new Vector3(eventData.delta.x, eventData.delta.y, 0);
             transform.SetParent(GameObject.Find("GamePanel").transform);
             transform.SetAsLastSibling();
+        }
+        else
+        {
+            if (Input.touchCount == 1)
+            {
+                transform.position = transform.position + new Vector3(eventData.delta.x, eventData.delta.y, 0);
+                transform.SetParent(GameObject.Find("GamePanel").transform);
+                transform.SetAsLastSibling();
+            }
         }
 
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (Input.touchCount == 1)
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             Debug.Log("PointerDown");
             gameAPI.VibrateWeak();
             gameAPI.PlaySFX("Pickup");
             parentName = transform.parent.name;
         }
-
+        else
+        {
+            if (Input.touchCount == 1)
+            {
+                Debug.Log("PointerDown");
+                gameAPI.VibrateWeak();
+                gameAPI.PlaySFX("Pickup");
+                parentName = transform.parent.name;
+            }
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)

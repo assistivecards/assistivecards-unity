7e5c202aeb10d0cb91002b1e81840e314f888c0d
Özlem <88405137+ogucukbel@users.noreply.github.com>
2024-01-03 15:21:43 +0300

    Change Movement Input Control On Card Maze

--- Assets/Scripts/Games/Card Maze/CardMazeDraggableCard.cs
+++ Assets/Scripts/Games/Card Maze/CardMazeDraggableCard.cs
@@ -13,41 +13,31 @@ public class CardMazeDraggableCard : MonoBehaviour
         gameAPI = Camera.main.GetComponent<GameAPI>();
     }
 
+    void OnMouseDrag()
+    {
+        var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPosition = new Vector3(wp.x, wp.y, transform.position.z);
+
+        GetComponent<Rigidbody2D>().MovePosition(newPosition);
+    }
+
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
+
+            var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var touchPosition = new Vector2(wp.x, wp.y);
+
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
             {
-
-                var wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                var touchPosition = new Vector2(wp.x, wp.y);
-
-                if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
-                {
-                    isValid = true;
-                    gameAPI.PlaySFX("Pickup");
-                }
-
-                else
-                {
-                    Debug.Log("MISS");
-                }
-
+                isValid = true;
+                gameAPI.PlaySFX("Pickup");
             }
 
-            if (Input.GetTouch(0).phase == TouchPhase.Moved && isValid)
+            else
             {
-
-                var wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                newPosition = new Vector3(wp.x, wp.y, transform.position.z);
-
-                GetComponent<Rigidbody2D>().MovePosition(newPosition);
-            }
-
-            if (Input.GetTouch(0).phase == TouchPhase.Ended && isValid)
-            {
-                isValid = false;
+                Debug.Log("MISS");
             }
 
         }

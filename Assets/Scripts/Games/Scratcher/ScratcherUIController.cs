e14a91bff45f7b0727b5fdc6a15c54fcb797cdfa
Özlem <88405137+ogucukbel@users.noreply.github.com>
2024-01-05 15:51:58 +0300

    Update Tutorial Activation Status On Scratcher

--- Assets/Scripts/Games/Scratcher/ScratcherUIController.cs
+++ Assets/Scripts/Games/Scratcher/ScratcherUIController.cs
@@ -33,6 +33,11 @@ public class ScratcherUIController : MonoBehaviour
         firstTime = false;
     }
 
+    public void TutorialSetDeactive()
+    {
+        tutorial.SetActive(false);
+    }
+
     public void OnBackButtonClick()
     {
         if (SystemInfo.deviceType == DeviceType.Desktop)
@@ -65,7 +70,7 @@ public class ScratcherUIController : MonoBehaviour
         speakerIcon.SetActive(true);
         homeButton.SetActive(true);
         levelProgressContainer.SetActive(true);
-
+        TutorialSetDeactive();
     }
 
     public void EnableBackButton()
@@ -92,6 +97,7 @@ public class ScratcherUIController : MonoBehaviour
         LeanTween.scale(checkPointPanel, Vector3.one * 0.6f, 0.25f);
         gameAPI.PlaySFX("Finished");
         Invoke("EnableContinuePlayingButton", .75f);
+        TutorialSetDeactive();
     }
 
     public void CloseCheckpointPanel()

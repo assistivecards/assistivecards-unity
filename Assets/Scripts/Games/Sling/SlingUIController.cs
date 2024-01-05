404d030ff355600acfbdce52f8dcaf344181e5ee
Özlem <88405137+ogucukbel@users.noreply.github.com>
2024-01-05 17:43:57 +0300

    Update Tutorial Activation Status On Sling

--- Assets/Scripts/Games/Sling/SlingUIController.cs
+++ Assets/Scripts/Games/Sling/SlingUIController.cs
@@ -32,6 +32,11 @@ public class SlingUIController : MonoBehaviour
         firstTime = false;
     }
 
+    public void TutorialSetDeactive()
+    {
+        tutorial.SetActive(false);
+    }
+
     public void OnBackButtonClick()
     {        
         if (SystemInfo.deviceType == DeviceType.Desktop)
@@ -66,7 +71,7 @@ public class SlingUIController : MonoBehaviour
         speakerIcon.SetActive(true);
         homeButton.SetActive(true);
         levelProgressContainer.SetActive(true);
-
+        TutorialSetDeactive();
     }
 
     public void EnableScrollRect()
@@ -82,6 +87,7 @@ public class SlingUIController : MonoBehaviour
         LeanTween.scale(checkPointPanel, Vector3.one * 0.6f, 0.25f);
         gameAPI.PlaySFX("Finished");
         Invoke("EnableContinuePlayingButton", .75f);
+        TutorialSetDeactive();
     }
 
     public void CloseCheckpointPanel()

03e12af51644756cf372057d4e3544c084af505f
Özlem <88405137+ogucukbel@users.noreply.github.com>
2024-01-08 15:32:22 +0300

    Change Input Detection On Sling

--- Assets/Scripts/Games/Sling/SwipeManager.cs
+++ Assets/Scripts/Games/Sling/SwipeManager.cs
@@ -17,6 +17,7 @@ public class SwipeManager : MonoBehaviour
     Vector3 newPosition;
     public float speedMultiplier;
     public bool isBeingDragged;
+    public bool isMouseDown = false;
     private GameAPI gameAPI;
     public bool isGrabbed;
 
@@ -34,13 +35,16 @@ public class SwipeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
+        // if (Input.touchCount > 0)
+        // {
+            if (Input.GetMouseButtonDown(0))
             {
                 // Debug.Log("Began");
+                isMouseDown = true;
                 pastPositions.Clear();
-                var wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 var touchPosition = new Vector2(wp.x, wp.y);
 
                 // if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
@@ -48,7 +52,7 @@ public class SwipeManager : MonoBehaviour
                 {
                     isValid = true;
                     touchTimeStart = Time.time;
-                    startPos = Input.GetTouch(0).position;
+                    startPos = Input.mousePosition;
                 }
 
                 else
@@ -58,30 +62,13 @@ public class SwipeManager : MonoBehaviour
 
             }
 
-            if (Input.GetTouch(0).phase == TouchPhase.Moved && canThrow && isValid)
-            {
-                // Debug.Log("Moved");
+            OnMouseDrag();
 
-                if (!isGrabbed)
-                {
-                    gameAPI.PlaySFX("Pickup");
-                    isGrabbed = true;
-                }
-                isBeingDragged = true;
-                var wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                newPosition = new Vector3(wp.x, wp.y, transform.position.z);
-
-                pastPositions.Enqueue(newPosition);
-                transform.position = newPosition;
-
-                // Debug.Log(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position));
-            }
-
-            if (Input.GetTouch(0).phase == TouchPhase.Ended && canThrow && isValid)
+            if (Input.GetMouseButtonUp(0) && canThrow && isValid)
             {
                 // Debug.Log("Ended");
                 isBeingDragged = false;
-
+                isMouseDown = false;
                 // touchTimeFinish = Time.time;
                 // timeInterval = touchTimeFinish - touchTimeStart;
 
@@ -102,8 +89,84 @@ public class SwipeManager : MonoBehaviour
             }
 
         }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    // Debug.Log("Began");
+                    isMouseDown = true;
+                    pastPositions.Clear();
+                    var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    var touchPosition = new Vector2(wp.x, wp.y);
+
+                    // if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
+                    if (GetComponent<Collider2D>().OverlapPoint(touchPosition))
+                    {
+                        isValid = true;
+                        touchTimeStart = Time.time;
+                        startPos = Input.mousePosition;
+                    }
+
+                    else
+                    {
+                        Debug.Log("MISS");
+                    }
+
+                }
+
+                OnMouseDrag();
+
+                if (Input.GetMouseButtonUp(0) && canThrow && isValid)
+                {
+                    // Debug.Log("Ended");
+                    isBeingDragged = false;
+                    isMouseDown = false;
+                    // touchTimeFinish = Time.time;
+                    // timeInterval = touchTimeFinish - touchTimeStart;
+
+                    // endPos = Input.GetTouch(0).position;
+                    // direction = startPos - endPos;
+
+                    rb.isKinematic = false;
+                    // rb.AddForce(-direction / timeInterval * throwForce);
+                    if (pastPositions.Count != 0)
+                    {
+                        var velocity = (newPosition - pastPositions.Peek()) / pastPositions.Count;
+                        transform.GetComponent<Rigidbody2D>().velocity = velocity * speedMultiplier;
+
+                    }
+
+                    canThrow = false;
+                    isGrabbed = false;
+                }
+            }
+        }
+    }
+    private void OnMouseDrag()
+    {
+        if(isMouseDown && canThrow && isValid)
+        {
+            // Debug.Log("Moved");
+
+            if (!isGrabbed)
+            {
+                gameAPI.PlaySFX("Pickup");
+                isGrabbed = true;
+            }
+            isBeingDragged = true;
+            var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPosition = new Vector3(wp.x, wp.y, transform.position.z);
+
+            pastPositions.Enqueue(newPosition);
+            transform.position = newPosition;
+
+            // Debug.Log(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position));
+        }
     }
 }
+
 
 class PositionQueue
 {

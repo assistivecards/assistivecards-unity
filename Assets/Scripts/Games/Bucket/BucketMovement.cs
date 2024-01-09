850e37210f8014543f9f7e2c1ac2a28e833f60e3
Özlem <88405137+ogucukbel@users.noreply.github.com>
2024-01-09 17:51:57 +0300

    Add Input Check To Bucket Game

--- Assets/Scripts/Games/Bucket/BucketMovement.cs
+++ Assets/Scripts/Games/Bucket/BucketMovement.cs
@@ -8,6 +8,16 @@ public class BucketMovement : MonoBehaviour, IDragHandler
 {
     public void OnDrag(PointerEventData eventData) 
     {
-        transform.position = new Vector3(eventData.position.x, this.transform.position.y, 1);
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            transform.position = new Vector3(eventData.position.x, this.transform.position.y, 1);
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                transform.position = new Vector3(eventData.position.x, this.transform.position.y, 1);
+            }
+		}
     }
 }
